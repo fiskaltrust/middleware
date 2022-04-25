79c6e31e715b55ab380207d758c5981b0c66bbe4
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2022-04-25 14:00:05 +0200

    Added skeleton for ME InMemory SCU

---
+++ scu-me/src/fiskaltrust.Middleware.SCU.ME.InMemory/InMemorySCU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using fiskaltrust.ifPOS.v2.me;
+
+namespace fiskaltrust.Middleware.SCU.ME.InMemory;
+
+public class InMemorySCU : IMESSCD
+{
+    public Task<RegisterCashDepositResponse> RegisterCashDepositAsync(RegisterCashDepositRequest registerCashDepositRequest) => throw new NotImplementedException();
+
+    public Task<RegisterInvoiceResponse> RegisterInvoiceAsync(RegisterInvoiceRequest registerInvoiceRequest) => throw new NotImplementedException();
+
+    public Task<RegisterTCRResponse> RegisterTCRAsync(RegisterTCRRequest registerTCRRequest) => throw new NotImplementedException();
+}
---
+++ scu-me/src/fiskaltrust.Middleware.SCU.ME.InMemory/InMemorySCUConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fiskaltrust.Middleware.SCU.ME.InMemory
+{
+    internal class InMemorySCUConfiguration
+    {
+        /// <summary>
+        /// Tax identification number (TIN) of the PosOperator.
+        /// </summary>
+        public string TIN { get; set; }
+
+        /// <summary>
+        /// VAT number of the PosOperator.
+        /// </summary>
+        public string VatNumber { get; set; }
+
+        /// <summary>
+        /// Name of the PosOperator.
+        /// </summary>
+        public string PosOperatorName { get; set; }
+
+        /// <summary>
+        /// Address of the PosOperator.
+        /// </summary>
+        public string PosOperatorAddress { get; set; }
+
+        /// <summary>
+        /// Town of the PosOperator.
+        /// </summary>
+        public string PosOperatorTown { get; set; }
+
+        /// <summary>
+        /// Country of the PosOperator.
+        /// </summary>
+        public string PosOperatorCountry { get; set; }
+    }
+}
