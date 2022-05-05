be8632d1c83abd503f40a692653522e0ee2ae34b
forsthug <85173816+forsthug@users.noreply.github.com>
2022-05-05 11:16:44 +0200

    Cashwithdrwal

--- queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/CashWithdrawlReceiptCommand.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/CashWithdrawlReceiptCommand.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Linq;
 using fiskaltrust.storage.V0;
 using Microsoft.Extensions.Logging;
 using fiskaltrust.Middleware.Localization.QueueME.Models;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.ifPOS.v1.me;
+using fiskaltrust.Middleware.Localization.QueueME.Exceptions;
 
 namespace fiskaltrust.Middleware.Localization.QueueME.RequestCommands
 {
@@ -17,10 +19,26 @@ namespace fiskaltrust.Middleware.Localization.QueueME.RequestCommands
         {
             try
             {
-
-                return await Task.FromResult(new RequestCommandResponse()
+                var queueMe = await _configurationRepository.GetQueueMEAsync(queue.ftQueueId).ConfigureAwait(false);
+                if (queueMe == null || !queueMe.ftSignaturCreationUnitMEId.HasValue)
                 {
-                });
+                    throw new ENUNotRegisteredException();
+                }
+                var scu = await _configurationRepository.GetSignaturCreationUnitMEAsync(queueMe.ftSignaturCreationUnitMEId.Value).ConfigureAwait(false);
+                var registerCashWithdrawalRequest = new RegisterCashWithdrawalRequest()
+                {
+                    Amount = request.cbReceiptAmount ?? request.cbChargeItems.Sum(x => x.Amount),
+                    Moment = request.cbReceiptMoment,
+                    RequestId = queueItem.ftQueueItemId,
+                    SubsequentDeliveryType = null,
+                    TcrCode = scu.TcrCode
+                };
+                await client.RegisterCashWithdrawalAsync(registerCashWithdrawalRequest).ConfigureAwait(false);
+                var receiptResponse = CreateReceiptResponse(request, queueItem);
+                return new RequestCommandResponse()
+                {
+                    ReceiptResponse = receiptResponse
+                };
             }
             catch (Exception ex) when (ex.GetType().Name == RETRYPOLICYEXCEPTION_NAME)
             {
