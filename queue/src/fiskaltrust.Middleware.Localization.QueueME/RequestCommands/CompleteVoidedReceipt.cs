87d323d3708e876003139c72334c75fdfaabb035
forsthug <85173816+forsthug@users.noreply.github.com>
2022-07-05 08:05:08 +0200

    partial void cash

--- queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/CompleteVoidedReceipt.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/CompleteVoidedReceipt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.ifPOS.v1.me;
@@ -16,16 +13,61 @@ namespace fiskaltrust.Middleware.Localization.QueueME.RequestCommands
 {
     public class CompleteVoidedReceipt : PosReceiptCommand
     {
-        private const string PreviousReceiptNotSet =
+        protected const string PreviousReceiptNotSet =
             "The field cbPreviousReceiptReference must be set to the cbReceiptReference of the receipt to cancel!";
         public CompleteVoidedReceipt(ILogger<RequestCommand> logger, IConfigurationRepository configurationRepository,
             IMiddlewareJournalMERepository journalMeRepository, IMiddlewareQueueItemRepository queueItemRepository, IMiddlewareActionJournalRepository actionJournalRepository, QueueMEConfiguration queueMeConfiguration) :
             base(logger, configurationRepository, journalMeRepository, queueItemRepository, actionJournalRepository, queueMeConfiguration)
         { }
 
-        public override Task<bool> ReceiptNeedsReprocessing(ftQueueME queueMe, ftQueueItem queueItem, ReceiptRequest request) => throw new NotImplementedException();
-
         public override async Task<RequestCommandResponse> ExecuteAsync(IMESSCD client, ftQueue queue, ReceiptRequest request, ftQueueItem queueItem, ftQueueME queueMe)
+        {
+            var (scu, _) = await ValidateVoidReceipt(request, queueItem, queueMe);
+            var queueItemToCancel = GetReceiptToCancel(request, out var journalMeToCancel, out var receiptRequestToCancel, out var invoiceToCancel);
+            return await CompleteVoidOfReceipt(client, queue, queueItem, queueMe, receiptRequestToCancel, invoiceToCancel, queueItemToCancel, journalMeToCancel, scu);
+        }
+
+        protected async Task<RequestCommandResponse> CompleteVoidOfReceipt(IMESSCD client, ftQueue queue, ftQueueItem queueItem, ftQueueME queueMe,
+            ReceiptRequest receiptRequestToCancel, Invoice invoiceToCancel, ftQueueItem queueItemToCancel,
+            ftJournalME journalMeToCancel, ftSignaturCreationUnitME scu)
+        {
+            var invoiceDetailsCancel =
+                await CreateInvoiceDetail(receiptRequestToCancel, invoiceToCancel, queueItemToCancel, true)
+                    .ConfigureAwait(false);
+            invoiceDetailsCancel.YearlyOrdinalNumber = await GetNextOrdinalNumber(queueItem).ConfigureAwait(false);
+            invoiceDetailsCancel.InvoiceCorrectionDetails = new InvoiceCorrectionDetails
+            {
+                ReferencedIKOF = journalMeToCancel.IIC,
+                ReferencedMoment = queueItemToCancel.cbReceiptMoment,
+                CorrectionType = InvoiceCorrectionType.Corrective
+            };
+            return await SendInvoiceDetailToCis(client, queue, receiptRequestToCancel, queueItem, queueMe, scu,
+                invoiceDetailsCancel, invoiceToCancel);
+        }
+
+        protected ftQueueItem GetReceiptToCancel(ReceiptRequest request, out ftJournalME journalMeToCancel,
+            out ReceiptRequest receiptRequestToCancel, out Invoice invoiceToCancel)
+        {
+            var queueItemToCancel = QueueItemRepository.GetByReceiptReferenceAsync(request.cbPreviousReceiptReference)
+                .ToListAsync().Result.FirstOrDefault();
+            if (queueItemToCancel == null)
+            {
+                throw new ReferenceNotSetException(PreviousReceiptNotSet);
+            }
+
+            journalMeToCancel = JournalMeRepository.GetByQueueItemId(queueItemToCancel.ftQueueItemId).ToListAsync()
+                .Result.FirstOrDefault();
+            if (journalMeToCancel?.IIC == null)
+            {
+                throw new InvoiceNotSignedException("Invoice was not signed. No IIC was created by the CA");
+            }
+
+            receiptRequestToCancel = JsonConvert.DeserializeObject<ReceiptRequest>(queueItemToCancel.request);
+            invoiceToCancel = JsonConvert.DeserializeObject<Invoice>(receiptRequestToCancel.ftReceiptCaseData);
+            return queueItemToCancel;
+        }
+
+        protected async Task<(ftSignaturCreationUnitME, Invoice)> ValidateVoidReceipt(ReceiptRequest request, ftQueueItem queueItem, ftQueueME queueMe)
         {
             var scu = await IsEnuRegistered(queueMe).ConfigureAwait(false);
             await CashDepositOutstanding().ConfigureAwait(false);
@@ -36,29 +78,8 @@ namespace fiskaltrust.Middleware.Localization.QueueME.RequestCommands
             {
                 throw new ReferenceNotSetException(PreviousReceiptNotSet);
             }
-            var queueItemToCancel = QueueItemRepository.GetByReceiptReferenceAsync(request.cbPreviousReceiptReference)
-                .ToListAsync().Result.FirstOrDefault();
-            if (queueItemToCancel == null)
-            {
-                throw new ReferenceNotSetException(PreviousReceiptNotSet);
-            }
-            var journalMeToCancel = JournalMeRepository.GetByQueueItemId(queueItemToCancel.ftQueueItemId).ToListAsync()
-                .Result.FirstOrDefault();
-            if (journalMeToCancel?.IIC == null)
-            {
-                throw new InvoiceNotSignedException("Invoice was not signed. No IIC was created by the CA");
-            }
-            var receiptRequestToCancel = JsonConvert.DeserializeObject<ReceiptRequest>(queueItemToCancel.request);
-            var invoiceToCancel = JsonConvert.DeserializeObject<Invoice>(receiptRequestToCancel.ftReceiptCaseData);
-            var invoiceDetailsCancel = await CreateInvoiceDetail(receiptRequestToCancel, invoiceToCancel, queueItemToCancel, true).ConfigureAwait(false);
-            invoiceDetailsCancel.YearlyOrdinalNumber = await GetNextOrdinalNumber(queueItem).ConfigureAwait(false);
-            invoiceDetailsCancel.InvoiceCorrectionDetails = new InvoiceCorrectionDetails
-            {
-                ReferencedIKOF = journalMeToCancel.IIC,
-                ReferencedMoment = queueItemToCancel.cbReceiptMoment,
-                CorrectionType = InvoiceCorrectionType.Corrective
-            };
-            return await SendInvoiceDetailToCis(client, queue, receiptRequestToCancel, queueItem, queueMe, scu, invoiceDetailsCancel, invoiceToCancel);
+
+            return (scu, invoice);
         }
     }
 }
--- queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/PartialVoidedReceipt.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueME/RequestCommands/PartialVoidedReceipt.cs
@@ -1,23 +1,36 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.ifPOS.v1.me;
 using fiskaltrust.Middleware.Contracts.Repositories;
+using fiskaltrust.Middleware.Localization.QueueME.Extensions;
 using fiskaltrust.Middleware.Localization.QueueME.Models;
 using fiskaltrust.storage.V0;
 using Microsoft.Extensions.Logging;
 
 namespace fiskaltrust.Middleware.Localization.QueueME.RequestCommands
 {
-    public class PartialVoidedReceipt : RequestCommand
+    public class PartialVoidedReceipt : CompleteVoidedReceipt
     {
         public PartialVoidedReceipt(ILogger<RequestCommand> logger, IConfigurationRepository configurationRepository,
             IMiddlewareJournalMERepository journalMeRepository, IMiddlewareQueueItemRepository queueItemRepository, IMiddlewareActionJournalRepository actionJournalRepository, QueueMEConfiguration queueMeConfiguration) :
             base(logger, configurationRepository, journalMeRepository, queueItemRepository, actionJournalRepository, queueMeConfiguration)
         { }
 
-        public override Task<bool> ReceiptNeedsReprocessing(ftQueueME queueMe, ftQueueItem queueItem, ReceiptRequest request) => throw new NotImplementedException();
+        public override async Task<RequestCommandResponse> ExecuteAsync(IMESSCD client, ftQueue queue, ReceiptRequest request, ftQueueItem queueItem, ftQueueME queueMe)
+        {
+            var (scu, invoice) = await ValidateVoidReceipt(request, queueItem, queueMe);
+            var queueItemToCancel = GetReceiptToCancel(request, out var journalMeToCancel, out var receiptRequestToCancel, out var invoiceToCancel);
+            if (receiptRequestToCancel.cbPayItems.Any(x => x.IsCashLocalCurrency()) ||
+                request.cbPayItems.Any(x => x.IsCashLocalCurrency()))
+            {
+                await CompleteVoidOfReceipt(client, queue, queueItem, queueMe, receiptRequestToCancel,
+                    invoiceToCancel, queueItemToCancel, journalMeToCancel, scu);
+                var invoiceDetails = await CreateInvoiceDetail(request, invoice, queueItem).ConfigureAwait(false);
+                return await SendInvoiceDetailToCis(client, queue, request, queueItem, queueMe, scu, invoiceDetails, invoice);
+            }
 
-        public override Task<RequestCommandResponse> ExecuteAsync(IMESSCD client, ftQueue queue, ReceiptRequest request, ftQueueItem queueItem, ftQueueME queueMe) => throw new NotImplementedException();
+            return null; //ToDo partial no cash
+        }
     }
 }
