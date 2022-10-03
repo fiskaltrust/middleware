f8754ecad6bc13b038d88afaeef6ff32bff98798
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2022-10-03 14:51:55 +0200

    Removed obsolete TODO

--- scu-de/src/fiskaltrust.Middleware.SCU.DE.Swissbit/Helpers/TimestampHelper.cs
+++ scu-de/src/fiskaltrust.Middleware.SCU.DE.Swissbit/Helpers/TimestampHelper.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace fiskaltrust.Middleware.SCU.DE.Swissbit.Helpers
 {
     public static class TimestampHelper
     {
-        // TODO add TimeFormat as parameter and share it over multiple scu implementations
-
         public static DateTime ToDateTime(this ulong timestamp) => new DateTime(1970, 1, 1).AddSeconds(timestamp);
 
         public static ulong ToTimestamp(this DateTime time) => (ulong) time.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
