9fc30260c93b9177e514bd2cbf8cf6084883db2b
forsthug <85173816+forsthug@users.noreply.github.com>
2022-12-01 08:15:53 +0100

    bulkinsert

--- queue/src/fiskaltrust.Middleware.Storage.SQLite/Repositories/SQLiteQueueItemRepository.cs
+++ queue/src/fiskaltrust.Middleware.Storage.SQLite/Repositories/SQLiteQueueItemRepository.cs
@@ -34,6 +34,93 @@ namespace fiskaltrust.Middleware.Storage.SQLite.Repositories
             await DbConnection.ExecuteAsync(sql, entity).ConfigureAwait(false);
         }
 
+        public async Task Insert(IAsyncEnumerable<ftQueueItem> ftQueueItems)
+        {
+            using (var transaction = DbConnection.BeginTransaction())
+            {
+                var command = DbConnection.CreateCommand();
+                command.CommandText = @"INSERT OR REPLACE INTO ftQueueItem " +
+                          "(ftQueueItemId, ftQueueId, ftQueueRow, ftQueueMoment, ftQueueTimeout, ftWorkMoment, ftDoneMoment, cbReceiptMoment, cbTerminalID, cbReceiptReference, country, version, request, requestHash, response, responseHash, TimeStamp) " +
+                          "Values (@ftQueueItemId, @ftQueueId, @ftQueueRow, @ftQueueMoment, @ftQueueTimeout, @ftWorkMoment, @ftDoneMoment, @cbReceiptMoment, @cbTerminalID, @cbReceiptReference, @country, @version, @request, @requestHash, @response, @responseHash, @TimeStamp);";
+
+                var ftQueueItemId = command.CreateParameter();
+                ftQueueItemId.ParameterName = "@ftQueueItemId";
+                command.Parameters.Add(ftQueueItemId);
+                var ftQueueId = command.CreateParameter();
+                ftQueueId.ParameterName = "@ftQueueId";
+                command.Parameters.Add(ftQueueId);
+                var ftQueueRow = command.CreateParameter();
+                ftQueueRow.ParameterName = "@ftQueueRow";
+                command.Parameters.Add(ftQueueRow);
+                var ftQueueMoment = command.CreateParameter();
+                ftQueueMoment.ParameterName = "@ftQueueMoment";
+                command.Parameters.Add(ftQueueMoment);
+                var ftQueueTimeout = command.CreateParameter();
+                ftQueueTimeout.ParameterName = "@ftQueueTimeout";
+                command.Parameters.Add(ftQueueTimeout);
+                var ftWorkMoment = command.CreateParameter();
+                ftWorkMoment.ParameterName = "@ftWorkMoment";
+                command.Parameters.Add(ftWorkMoment);
+                var ftDoneMoment = command.CreateParameter();
+                ftDoneMoment.ParameterName = "@ftDoneMoment";
+                command.Parameters.Add(ftDoneMoment);
+                var cbReceiptMoment = command.CreateParameter();
+                cbReceiptMoment.ParameterName = "@cbReceiptMoment";
+                command.Parameters.Add(cbReceiptMoment);
+                var cbTerminalID = command.CreateParameter();
+                cbTerminalID.ParameterName = "@cbTerminalID";
+                command.Parameters.Add(cbTerminalID);
+                var cbReceiptReference = command.CreateParameter();
+                cbReceiptReference.ParameterName = "@cbReceiptReference";
+                command.Parameters.Add(cbReceiptReference);
+                var country = command.CreateParameter();
+                country.ParameterName = "@country";
+                command.Parameters.Add(country);
+                var version = command.CreateParameter();
+                version.ParameterName = "@version";
+                command.Parameters.Add(version);
+                var request = command.CreateParameter();
+                request.ParameterName = "@request";
+                command.Parameters.Add(request);
+                var requestHash = command.CreateParameter();
+                requestHash.ParameterName = "@requestHash";
+                command.Parameters.Add(requestHash);
+                var response = command.CreateParameter();
+                response.ParameterName = "@response";
+                command.Parameters.Add(response);
+                var responseHash = command.CreateParameter();
+                responseHash.ParameterName = "@responseHash";
+                command.Parameters.Add(responseHash);
+                var TimeStamp = command.CreateParameter();
+                TimeStamp.ParameterName = "@TimeStamp";
+                command.Parameters.Add(TimeStamp);
+
+                await foreach (var item in ftQueueItems)
+                {
+                    ftQueueItemId.Value = item.ftQueueItemId;
+                    ftQueueId.Value = item.ftQueueId;
+                    ftQueueRow.Value = item.ftQueueRow;
+                    ftQueueMoment.Value = item.ftQueueMoment;
+                    ftQueueTimeout.Value = item.ftQueueTimeout;
+                    ftWorkMoment.Value = item.ftWorkMoment;
+                    ftDoneMoment.Value = item.ftDoneMoment;
+                    cbReceiptMoment.Value = item.cbReceiptMoment;
+                    cbTerminalID.Value = item.cbTerminalID;
+                    cbReceiptReference.Value = item.cbReceiptReference;
+                    country.Value = item.country;
+                    version.Value = item.version;
+                    request.Value = item.request;
+                    requestHash.Value = item.requestHash;
+                    response.Value = item.response;
+                    responseHash.Value = item.responseHash;
+                    TimeStamp.Value = item.TimeStamp;
+                    command.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
+            }
+        }
+
         public async IAsyncEnumerable<ftQueueItem> GetByReceiptReferenceAsync(string cbReceiptReference, string cbTerminalId)
         {
             var query = "Select * from ftQueueItem where cbReceiptReference = @cbReceiptReference";
