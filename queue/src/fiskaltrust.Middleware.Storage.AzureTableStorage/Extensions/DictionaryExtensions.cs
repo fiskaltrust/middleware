223403787f3843fc85ce4b6bc164e661247913f4
Stefan Kert <stefan.kert@sbg.at>
2022-12-06 16:35:42 +0100

    Fix

--- queue/src/fiskaltrust.Middleware.Storage.AzureTableStorage/Extensions/DictionaryExtensions.cs
+++ queue/src/fiskaltrust.Middleware.Storage.AzureTableStorage/Extensions/DictionaryExtensions.cs
@@ -8,9 +8,9 @@ namespace fiskaltrust.Middleware.Storage.AzureTableStorage.Extensions
         public static Dictionary<string, TValue> ConvertToCaseInSensitive<TValue>(this Dictionary<string, TValue> dictionary)
         {
             var resultDictionary = new Dictionary<string, TValue>(StringComparer.InvariantCultureIgnoreCase);
-            foreach (var (key, value) in dictionary)
+            foreach (var kvp in dictionary)
             {
-                resultDictionary.Add(key, value);
+                resultDictionary.Add(kvp.Key, kvp.Value);
             }
             return resultDictionary;
         }
