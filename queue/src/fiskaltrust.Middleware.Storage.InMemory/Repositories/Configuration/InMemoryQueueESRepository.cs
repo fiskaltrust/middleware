338ad752d97d348f40cd58933b54761e41110b84
forsthug <85173816+forsthug@users.noreply.github.com>
2023-02-01 11:50:23 +0100

    repsitories

---
+++ queue/src/fiskaltrust.Middleware.Storage.InMemory/Repositories/Configuration/InMemoryQueueESRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using fiskaltrust.storage.V0;
+
+namespace fiskaltrust.Middleware.Storage.InMemory.Repositories.Configuration
+{
+    public class InMemoryQueueESRepository : AbstractInMemoryRepository<Guid, ftQueueES>
+    {
+        public InMemoryQueueESRepository() : base(new List<ftQueueES>()) { }
+
+        public InMemoryQueueESRepository(IEnumerable<ftQueueES> data) : base(data) { }
+
+        protected override void EntityUpdated(ftQueueES entity) => entity.TimeStamp = DateTime.UtcNow.Ticks;
+
+        protected override Guid GetIdForEntity(ftQueueES entity) => entity.ftQueueESId;
+    }
+}
---
+++ queue/src/fiskaltrust.Middleware.Storage.SQLite/Repositories/Configuration/SQLiteSignaturCreationUnitESRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using fiskaltrust.storage.V0;
+
+namespace fiskaltrust.Middleware.Storage.SQLite.Repositories.Configuration
+{
+    public class SQLiteSignaturCreationUnitESRepository : AbstractSQLiteRepository<Guid, ftSignaturCreationUnitES>, IConfigurationItemRepository<ftSignaturCreationUnitES>
+    {
+        public SQLiteSignaturCreationUnitESRepository(ISqliteConnectionFactory connectionFactory, string path) : base(connectionFactory, path) { }
+
+        public override void EntityUpdated(ftSignaturCreationUnitES entity) => entity.TimeStamp = DateTime.UtcNow.Ticks;
+
+        public override async Task<ftSignaturCreationUnitES> GetAsync(Guid id) => await DbConnection.QueryFirstOrDefaultAsync<ftSignaturCreationUnitES>("Select * from ftSignaturCreationUnitES where ftSignaturCreationUnitESId = @SignaturCreationUnitES", new { SignaturCreationUnitES = id }).ConfigureAwait(false);
+
+        public override async Task<IEnumerable<ftSignaturCreationUnitES>> GetAsync() => await DbConnection.QueryAsync<ftSignaturCreationUnitES>("select * from ftSignaturCreationUnitES").ConfigureAwait(false);
+
+        public async Task InsertOrUpdateAsync(ftSignaturCreationUnitES entity)
+        {
+            EntityUpdated(entity);
+            var sql = "INSERT OR REPLACE INTO ftSignaturCreationUnitES " +
+                            "(ftSignaturCreationUnitESId, Url, TimeStamp) " +
+                            "Values (@ftSignaturCreationUnitESId, @Url, @TimeStamp);";
+            await DbConnection.ExecuteAsync(sql, entity).ConfigureAwait(false);
+        }
+
+        protected override Guid GetIdForEntity(ftSignaturCreationUnitES entity) => entity.ftSignaturCreationUnitESId;
+    }
+}
