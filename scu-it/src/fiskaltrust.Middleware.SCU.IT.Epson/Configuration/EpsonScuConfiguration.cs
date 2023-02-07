c949d11e8a40b308d33d73389c999c509a8191b7
forsthug <85173816+forsthug@users.noreply.github.com>
2023-02-07 13:50:10 +0100

    first step xml & config

--- scu-it/src/fiskaltrust.Middleware.SCU.IT.Epson/Configuration/EpsonScuConfiguration.cs
+++ scu-it/src/fiskaltrust.Middleware.SCU.IT.Epson/Configuration/EpsonScuConfiguration.cs
@@ -1,7 +1,65 @@
-﻿namespace fiskaltrust.Middleware.SCU.IT.Configuration
+﻿using System.Reflection.Emit;
+using System.Xml.Serialization;
+
+namespace fiskaltrust.Middleware.SCU.IT.Configuration
 {
     public class EpsonScuConfiguration
     {
+        /// <summary>
+        /// position defines the starting position from the left margin (range 0 to 511). Three special values 
+        /// can also be used:
+        /// o 900 = Left aligned
+        /// o 901 = Centred
+        ///  o 902 = Right aligned
+        /// </summary>
+        public int BarCodePosition { get; set; }
+
+        /// <summary>
+        ///  Indicates the print dot width of each distinct bar (range 1 to 6). Please note that not all readers are able to read 
+        /// barcodes with a 1 dot width
+        /// </summary>
+        public int BarCodeWidth { get; set; }
+
+        /// <summary>
+        ///  Indicates the height of the bar code measured in print dots (range 1 to 255).
+        /// </summary>
+        public int BarCodeHeight { get; set; }
+
+        /// <summary>
+        ///  selects one of three ways to print the alphanumeric representation of the barcode 
+        /// or to disable it altogether.
+        /// The options are as follows:
+        /// o 0 = Disabled
+        /// o 1 = Above the barcode
+        /// o 2 = Below the barcode
+        /// o 3 = Below and above the barcode
+        /// </summary>
+        public int BarCodeHRIPosition { get; set; }
+
+        /// <summary>
+        ///  Indicates the font to be used for the HRI string.The options are as follows:
+        ///  o A
+        ///  o B
+        ///  o C
+        /// </summary>
+        public char BarCodeHRIFont { get; set; }
+
+        /// <summary>
+        ///  Indicates the barcode or QR code standard.Choose from one of the following:
+        ///  o UPC-A => 65
+        ///  o UPC-E / 66
+        ///  o EAN13 / 67
+        ///  o EAN8 / 68
+        ///  o CODE39 / 69
+        ///  o ITF / 70
+        ///  o CODABAR / 71
+        ///  o CODE93 / 72
+        ///  o CODE128 / 73
+        ///  o 74 to 78 *
+        ///  o QRCODE1 / 91
+        ///  o QRCODE2 / 92
+        /// </summary>
+        public int CodeType { get; set; }
 
     }
 }
---
+++ scu-it/src/fiskaltrust.Middleware.SCU.IT.Epson/Models/FiscalReceipt.cs
@@ -0,0 +1,188 @@
+﻿
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace fiskaltrust.Middleware.SCU.IT.Epson.Models
+{
+    [XmlRoot(ElementName = "displayText")]
+    public class DisplayText
+    {
+        public DisplayText(string data) => Data = data;
+
+        [XmlAttribute(AttributeName = "operator")]
+        public string? Operator { get; set; }
+        [XmlAttribute(AttributeName = "data")]
+        public string Data { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecMessage")]
+    public class PrintRecMessage
+    {
+        public PrintRecMessage(string message) => Message = message;
+
+        [XmlAttribute(AttributeName = "operator")]
+        public string? Operator { get; set; }
+        [XmlAttribute(AttributeName = "messageType")]
+        public string? MessageType { get; set; }
+        [XmlAttribute(AttributeName = "index")]
+        public string? Index { get; set; }
+        [XmlAttribute(AttributeName = "font")]
+        public string? Font { get; set; }
+        [XmlAttribute(AttributeName = "message")]
+        public string Message { get; set; }
+        [XmlAttribute(AttributeName = "comment")]
+        public string? Comment { get; set; }
+    }
+
+    [XmlRoot(ElementName = "beginFiscalReceipt")]
+    public class BeginFiscalReceipt
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string? Operator { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecItem")]
+    public class PrintRecItem
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string? Operator { get; set; }
+        [XmlAttribute(AttributeName = "description")]
+        public string? Description { get; set; }
+        [XmlAttribute(AttributeName = "quantity")]
+        public string? Quantity { get; set; }
+        [XmlAttribute(AttributeName = "unitPrice")]
+        public string? UnitPrice { get; set; }
+        [XmlAttribute(AttributeName = "department")]
+        public string? Department { get; set; }
+        [XmlAttribute(AttributeName = "justification")]
+        public string? Justification { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecItemVoid")]
+    public class PrintRecItemVoid
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+        [XmlAttribute(AttributeName = "quantity")]
+        public string Quantity { get; set; }
+        [XmlAttribute(AttributeName = "unitPrice")]
+        public string UnitPrice { get; set; }
+        [XmlAttribute(AttributeName = "department")]
+        public string Department { get; set; }
+        [XmlAttribute(AttributeName = "justification")]
+        public string Justification { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecItemAdjustment")]
+    public class PrintRecItemAdjustment
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+        [XmlAttribute(AttributeName = "adjustmentType")]
+        public string AdjustmentType { get; set; }
+        [XmlAttribute(AttributeName = "amount")]
+        public string Amount { get; set; }
+        [XmlAttribute(AttributeName = "justification")]
+        public string Justification { get; set; } = "1";
+    }
+
+    [XmlRoot(ElementName = "printRecSubtotalAdjustment")]
+    public class PrintRecSubtotalAdjustment
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+        [XmlAttribute(AttributeName = "adjustmentType")]
+        public string AdjustmentType { get; set; }
+        [XmlAttribute(AttributeName = "amount")]
+        public string Amount { get; set; }
+        [XmlAttribute(AttributeName = "justification")]
+        public string Justification { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecSubtotal")]
+    public class PrintRecSubtotal
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "option")]
+        public string Option { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printBarCode")]
+    public class PrintBarCode
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "position")]
+        public string Position { get; set; }
+        [XmlAttribute(AttributeName = "width")]
+        public string Width { get; set; }
+        [XmlAttribute(AttributeName = "height")]
+        public string Height { get; set; }
+        [XmlAttribute(AttributeName = "hRIPosition")]
+        public string HRIPosition { get; set; }
+        [XmlAttribute(AttributeName = "hRIFont")]
+        public string HRIFont { get; set; }
+        [XmlAttribute(AttributeName = "codeType")]
+        public string CodeType { get; set; }
+        [XmlAttribute(AttributeName = "code")]
+        public string Code { get; set; }
+    }
+
+    [XmlRoot(ElementName = "printRecTotal")]
+    public class PrintRecTotal
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; }
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+        [XmlAttribute(AttributeName = "payment")]
+        public string Payment { get; set; }
+        [XmlAttribute(AttributeName = "paymentType")]
+        public string PaymentType { get; set; }
+        [XmlAttribute(AttributeName = "index")]
+        public string Index { get; set; }
+        [XmlAttribute(AttributeName = "justification")]
+        public string Justification { get; set; }
+    }
+
+    [XmlRoot(ElementName = "endFiscalReceipt")]
+    public class EndFiscalReceipt
+    {
+        [XmlAttribute(AttributeName = "operator")]
+        public string Operator { get; set; } 
+    }
+
+    [XmlRoot(ElementName = "printerFiscalReceipt")]
+    public class FiscalReceipt
+    {
+        [XmlElement(ElementName = "displayText")]
+        public List<DisplayText> DisplayText { get; set; }
+        [XmlElement(ElementName = "printRecMessage")]
+        public List<PrintRecMessage> PrintRecMessage { get; set; }
+        [XmlElement(ElementName = "beginFiscalReceipt")]
+        public BeginFiscalReceipt BeginFiscalReceipt { get; set; }
+        [XmlElement(ElementName = "printRecItem")]
+        public List<PrintRecItem> PrintRecItem { get; set; }
+        [XmlElement(ElementName = "printRecItemVoid")]
+        public PrintRecItemVoid PrintRecItemVoid { get; set; }
+        [XmlElement(ElementName = "printRecItemAdjustment")]
+        public PrintRecItemAdjustment PrintRecItemAdjustment { get; set; }
+        [XmlElement(ElementName = "printRecSubtotalAdjustment")]
+        public PrintRecSubtotalAdjustment PrintRecSubtotalAdjustment { get; set; }
+        [XmlElement(ElementName = "printRecSubtotal")]
+        public PrintRecSubtotal PrintRecSubtotal { get; set; }
+        [XmlElement(ElementName = "printBarCode")]
+        public PrintBarCode PrintBarCode { get; set; }
+        [XmlElement(ElementName = "printRecTotal")]
+        public PrintRecTotal PrintRecTotal { get; set; }
+        [XmlElement(ElementName = "endFiscalReceipt")]
+        public EndFiscalReceipt EndFiscalReceipt { get; set; }
+    }
+
+}
