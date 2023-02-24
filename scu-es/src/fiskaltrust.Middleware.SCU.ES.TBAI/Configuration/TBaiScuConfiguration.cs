bbc42e86b688211d8a7e3362e5dc6c2092fcc25d
forsthug <85173816+forsthug@users.noreply.github.com>
2023-02-24 11:56:52 +0100

    Create Skeleton of Tbai-Scu

---
+++ scu-es/src/fiskaltrust.Middleware.SCU.ES.TBAI/Configuration/TBaiScuConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Serialization;
+
+namespace fiskaltrust.Middleware.SCU.ES.Configuration
+{
+    public class TBaiScuConfiguration
+    {
+
+    }
+}
---
+++ scu-es/src/fiskaltrust.Middleware.SCU.ES.TBAI/ScuBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using fiskaltrust.Middleware.Abstractions;
+using fiskaltrust.Middleware.SCU.ES.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace fiskaltrust.Middleware.SCU.ES.TBAI;
+
+public class ScuBootstrapper : IMiddlewareBootstrapper
+{
+    public Guid Id { get; set; }
+    public Dictionary<string, object> Configuration { get; set; } = null!;
+
+    public void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        var tbaiScuConfig = JsonConvert.DeserializeObject<TBaiScuConfiguration>(JsonConvert.SerializeObject(Configuration));
+
+        _ = serviceCollection
+            .AddSingleton(tbaiScuConfig);
+            //.AddScoped<IESSSCD, TBaiSCU>();
+    }
+}
