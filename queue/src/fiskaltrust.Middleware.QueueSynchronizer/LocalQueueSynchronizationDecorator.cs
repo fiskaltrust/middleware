90636fbbf71d06f8c1d7f3ee05e466899664ee26
Paul Volavsek <paul.volavsek@gmail.com>
2023-04-28 11:56:17 +0200

    use channels implementation only on newer .net versions

--- queue/src/fiskaltrust.Middleware.QueueSynchronizer/LocalQueueSynchronizationDecorator.cs
+++ queue/src/fiskaltrust.Middleware.QueueSynchronizer/LocalQueueSynchronizationDecorator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.Middleware.Contracts;
@@ -8,6 +9,73 @@ using Microsoft.Extensions.Logging;
 
 namespace fiskaltrust.Middleware.QueueSynchronizer
 {
+#if NET461
+    public sealed class LocalQueueSynchronizationDecorator : ISignProcessor, IDisposable
+    {
+        private readonly ISignProcessor _signProcessor;
+        private readonly ILogger<LocalQueueSynchronizationDecorator> _logger;
+        private readonly BlockingCollection<(Guid identifier, ReceiptRequest request)> _concurrentQueue;
+        private readonly ConcurrentDictionary<Guid, SynchronizedResult> _stateDictionary;
+
+        public LocalQueueSynchronizationDecorator(ISignProcessor signProcessor, ILogger<LocalQueueSynchronizationDecorator> logger)
+        {
+            _signProcessor = signProcessor;
+            _logger = logger;
+            _concurrentQueue = new BlockingCollection<(Guid, ReceiptRequest)>(new ConcurrentQueue<(Guid, ReceiptRequest)>());
+            _stateDictionary = new ConcurrentDictionary<Guid, SynchronizedResult>();
+
+            _ = Task.Run(ProcessReceipts);
+        }
+
+        public async Task<ReceiptResponse> ProcessAsync(ReceiptRequest receiptRequest)
+        {
+            _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessAsync called.");
+            var identifier = Guid.NewGuid();
+            var syncResult = new SynchronizedResult();
+            _stateDictionary.TryAdd(identifier, syncResult);
+            _concurrentQueue.Add((identifier, receiptRequest));
+
+            _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessAsync: Waiting until result is available.");
+            await syncResult.AutoResetEvent.WaitAsync();
+            if (!_stateDictionary.TryRemove(identifier, out var synchronizedResult))
+            {
+                throw new KeyNotFoundException(identifier.ToString());
+            }
+
+            _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessAsync: Got receipt result.");
+            synchronizedResult.ExceptionDispatchInfo?.Throw();
+
+            return await Task.FromResult(synchronizedResult.Response).ConfigureAwait(false);
+        }
+
+        private async Task ProcessReceipts()
+        {
+            while (!_concurrentQueue.IsCompleted && _concurrentQueue.TryTake(out var tuple, -1))
+            {
+                _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessReceipts: Processing a new receipt.");
+                try
+                {
+                    var response = await _signProcessor.ProcessAsync(tuple.request).ConfigureAwait(false);
+                    _stateDictionary[tuple.identifier].Response = response;
+                }
+                catch (Exception ex)
+                {
+                    _stateDictionary[tuple.identifier].ExceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
+                }
+                finally
+                {
+                    _stateDictionary[tuple.identifier].AutoResetEvent.Set();
+                }
+            }
+        }
+
+        public void Dispose() => _concurrentQueue.Dispose();
+    }
+
+#else
+using System.Threading;
+using System.Threading.Channels;
+
     public sealed class LocalQueueSynchronizationDecorator : ISignProcessor, IDisposable
     {
         private readonly ISignProcessor _signProcessor;
@@ -20,6 +88,7 @@ namespace fiskaltrust.Middleware.QueueSynchronizer
             _signProcessor = signProcessor;
             _logger = logger;
             _channel = Channel.CreateUnbounded<(ReceiptRequest, ChannelWriter<SynchronizedResult>)>();
+
             _ = Task.Run(ProcessReceipts);
         }
 
@@ -27,6 +96,12 @@ namespace fiskaltrust.Middleware.QueueSynchronizer
         {
             _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessAsync called.");
             var responseChannel = Channel.CreateBounded<SynchronizedResult>(1);
+
+            if (!await _channel.Writer.WaitToWriteAsync())
+            {
+                throw new ObjectDisposedException(nameof(ISignProcessor), "Queue was already disposed");
+            }
+
             await _channel.Writer.WriteAsync((receiptRequest, responseChannel.Writer));
 
             _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessAsync: Waiting until result is available.");
@@ -51,6 +126,7 @@ namespace fiskaltrust.Middleware.QueueSynchronizer
 
                 _logger.LogTrace("LocalQueueSynchronizationDecorator.ProcessReceipts: Processing a new receipt.");
                 var synchronizedResult = new SynchronizedResult();
+
                 try
                 {
                     if (_disposed)
@@ -79,4 +155,5 @@ namespace fiskaltrust.Middleware.QueueSynchronizer
             _channel.Writer.Complete();
         }
     }
+#endif
 }
