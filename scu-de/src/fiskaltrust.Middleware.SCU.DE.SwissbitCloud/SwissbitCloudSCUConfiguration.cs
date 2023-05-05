0244b22604b80b666b08db65421427761588cba3
forsthug <85173816+forsthug@users.noreply.github.com>
2023-05-05 06:08:02 +0200

    Update Certificate

--- scu-de/src/fiskaltrust.Middleware.SCU.DE.SwissbitCloud/SwissbitCloudSCUConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using fiskaltrust.Middleware.SCU.DE.DeutscheFiskal;
-
-namespace fiskaltrust.Middleware.SCU.DE.SwissbitCloud
-{
-    public class SwissbitCloudSCUConfiguration : DeutscheFiskalSCUConfiguration
-    {
-        public override string CertificationId { get; set; } = "BSI-K-TR-0456-2021";
-    }
-}
