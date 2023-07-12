be137ebe6b6efc4f60c9a9964710701d73b6ebf2
Stefan Kert <stefan.kert@sbg.at>
2023-07-12 17:44:25 +0200

    Include path in test

--- scu-es/test/fiskaltrust.Middleware.SCU.ES.UnitTest/TicketBaiRequestFactoryTests.cs
+++ scu-es/test/fiskaltrust.Middleware.SCU.ES.UnitTest/TicketBaiRequestFactoryTests.cs
@@ -32,10 +32,10 @@ namespace fiskaltrust.Middleware.SCU.ES.UnitTest
 
 
         [Theory]
-        [InlineData("Example_bizkaia_TicketBAI_79732487C_A2022_0399.xml", "Example_bizkaia_TicketBAI_79732487C_A2022_0399_signed.xml")]
-        [InlineData("Example_bizkaia_TicketBAI_79732487C_A2022_0400.xml", "Example_bizkaia_TicketBAI_79732487C_A2022_0400_signed.xml")]
-        [InlineData("Example_bizkaia_TicketBAI_B00000034_B2022_0100.xml", "Example_bizkaia_TicketBAI_B00000034_B2022_0100_signed.xml")]
-        [InlineData("Example_bizkaia_TicketBAI_B00000034_B2022_0101.xml", "Example_bizkaia_TicketBAI_B00000034_B2022_0101_signed.xml")]
+        [InlineData("TestData/Example_bizkaia_TicketBAI_79732487C_A2022_0399.xml", "TestData/Example_bizkaia_TicketBAI_79732487C_A2022_0399_signed.xml")]
+        [InlineData("TestData/Example_bizkaia_TicketBAI_79732487C_A2022_0400.xml", "TestData/Example_bizkaia_TicketBAI_79732487C_A2022_0400_signed.xml")]
+        [InlineData("TestData/Example_bizkaia_TicketBAI_B00000034_B2022_0100.xml", "TestData/Example_bizkaia_TicketBAI_B00000034_B2022_0100_signed.xml")]
+        [InlineData("TestData/Example_bizkaia_TicketBAI_B00000034_B2022_0101.xml", "TestData/Example_bizkaia_TicketBAI_B00000034_B2022_0101_signed.xml")]
         public void SignXMLTest_ShouldCreateXadesBasedSignature(string xmlFile, string signedXmlFile)
         {
             var cert = new X509Certificate2(@"TestCertificates/autonomo_act.p12", "Iz3np32023", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet);
