804c0e17d50a243303a12e66e13c6eccfa0bf88c
Stefan Kert <stefan.kert@sbg.at>
2023-07-13 08:42:48 +0200

    Added response

---
+++ scu-es/src/fiskaltrust.Middleware.SCU.ES.TicketBAI/Models/TicketBaiResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable disable
+
+namespace fiskaltrust.Middleware.SCU.ES.TicketBAI.Models;
+
+[Serializable()]
+[System.ComponentModel.DesignerCategory("code")]
+[System.Xml.Serialization.XmlType(AnonymousType = true, Namespace = "urn:ticketbai:emision")]
+[System.Xml.Serialization.XmlRoot(Namespace = "urn:ticketbai:emision", IsNullable = false)]
+public class TicketBaiResponse
+{
+    [System.Xml.Serialization.XmlElement(Namespace = "")]
+    public Salida Salida { get; set; }
+}
+
+/// <remarks/>
+[Serializable()]
+[System.ComponentModel.DesignerCategory("code")]
+[System.Xml.Serialization.XmlType(AnonymousType = true)]
+[System.Xml.Serialization.XmlRoot(Namespace = "", IsNullable = false)]
+public partial class Salida
+{
+    public string IdentificadorTBAI { get; set; }
+
+    public string FechaRecepcion { get; set; }
+
+    public string Estado { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public string Azalpena { get; set; }
+
+    public string CSV { get; set; }
+}
--- scu-es/src/fiskaltrust.Middleware.SCU.ES.TicketBAI/TicketBaiSCU.cs
+++ scu-es/src/fiskaltrust.Middleware.SCU.ES.TicketBAI/TicketBaiSCU.cs
@@ -1,6 +1,11 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
 using fiskaltrust.Middleware.SCU.ES.TicketBAI.Models;
 using Microsoft.Extensions.Logging;
 
@@ -22,22 +27,60 @@ public sealed class TicketBaiSCU //: IESSSCD
         handler.ClientCertificates.Add(_configuration.Certificate);
         _httpClient = new HttpClient(handler)
         {
-            BaseAddress = new System.Uri(Digests.Gipuzkoa.SANDBOX_ENDPOINT)
+            BaseAddress = new Uri(Digests.Gipuzkoa.SANDBOX_ENDPOINT)
         };
     }
 
-    public async Task<string> SubmitInvoiceAsync(TicketBaiRequest ticketBaiRequest)
+    public async Task<TicketBaiResult> SubmitInvoiceAsync(TicketBaiRequest ticketBaiRequest)
     {
         var content = _ticketBaiRequestFactory.CreateXadesSignedXmlContent(ticketBaiRequest, Digests.Gipuzkoa.POLICY_IDENTIFIER, Digests.Gipuzkoa.POLICY_DIGEST, Digests.Gipuzkoa.POLICY_IDENTIFIER);
         var response = await _httpClient.PostAsync(Digests.Gipuzkoa.SUBMIT_INVOICES, new StringContent(content, Encoding.UTF8, "application/xml"));
-        if (response.IsSuccessStatusCode)
+        if (!response.IsSuccessStatusCode)
         {
             _logger.LogInformation("Successful");
         }
-        return await response.Content.ReadAsStringAsync();
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var ticketBaiResponse = ParseHelpers.ParseXML<TicketBaiResponse>(responseContent) ?? throw new Exception("Something horrible has happened");
+        if (ticketBaiResponse.Salida.Estado == "00")
+        {
+            var identifier = ticketBaiResponse.Salida.IdentificadorTBAI.Split('-');
+            var result =  new TicketBaiResult
+            {
+                IssuerVatId = identifier[1],
+                ExpeditionDate = identifier[2],
+                ShortSignatureValue = identifier[3],
+                Identifier = ticketBaiResponse.Salida.IdentificadorTBAI,
+                Content = responseContent,
+                Succeeded = true,
+            };
+            var crc8 = new CRC8Calculator();
+            var url = $"{GetTerritoryUrl()}?{IdentifierUrl(ticketBaiResponse.Salida.IdentificadorTBAI, ticketBaiRequest)}";
+            var cr8 = crc8.ComputeChecksum(url).ToString();
+            url += $"&cr={cr8.PadLeft(3, '0')}";
+            result.QrCode = new Uri(url);
+            return result;
+        }
+        else
+        {
+            return new TicketBaiResult
+            {
+                Content = responseContent,
+                Succeeded = false
+            };
+        }
     }
 
-    public async Task<string> CancelInvoiceAsync(TicketBaiRequest ticketBaiRequest)
+    private string IdentifierUrl(string ticketBaiIdentifier, TicketBaiRequest ticketBaiRequest)
+    {
+        return string.Format("id={0}&s={1}&nf={2}&i={3}",
+            HttpUtility.UrlEncode(ticketBaiIdentifier),
+            HttpUtility.UrlEncode(ticketBaiRequest.Factura.CabeceraFactura.SerieFactura),
+            HttpUtility.UrlEncode(ticketBaiRequest.Factura.CabeceraFactura.NumFactura),
+            HttpUtility.UrlEncode(ticketBaiRequest.Factura.DatosFactura.ImporteTotalFactura)
+        ); 
+    }
+
+public async Task<string> CancelInvoiceAsync(TicketBaiRequest ticketBaiRequest)
     {
         var content = _ticketBaiRequestFactory.CreateXadesSignedXmlContent(ticketBaiRequest, Digests.Gipuzkoa.POLICY_IDENTIFIER, Digests.Gipuzkoa.POLICY_DIGEST, Digests.Gipuzkoa.POLICY_IDENTIFIER);
         var response = await _httpClient.PostAsync(Digests.Gipuzkoa.CANCEL_INVOICES, new StringContent(content, Encoding.UTF8, "application/xml"));
@@ -47,4 +90,63 @@ public sealed class TicketBaiSCU //: IESSSCD
         }
         return await response.Content.ReadAsStringAsync();
     }
+
+
+    public Uri GetTerritoryUrl()
+    {
+        return new Uri("https://tbai.prep.gipuzkoa.eus/qr/");
+    }
+}
+
+public class CRC8Calculator
+{
+    private readonly byte[] table = new byte[256];
+
+    public CRC8Calculator()
+    {
+        GenerateTable();
+    }
+
+    public byte ComputeChecksum(string input)
+    {
+        var data = Encoding.UTF8.GetBytes(input);
+        byte crc = 0;
+        foreach (var b in data)
+        {
+            crc = table[crc ^ b];
+        }
+        return crc;
+    }
+
+    private void GenerateTable()
+    {
+        byte polynomial = 0x07;
+        for (var i = 0; i < 256; i++)
+        {
+            var temp = (byte) i;
+            for (byte j = 0; j < 8; j++)
+            {
+                if ((temp & 0x80) != 0)
+                {
+                    temp = (byte) ((temp << 1) ^ polynomial);
+                }
+                else
+                {
+                    temp <<= 1;
+                }
+            }
+            table[i] = temp;
+        }
+    }
+}
+
+public class TicketBaiResult
+{
+    public string? Content { get; set; }
+    public bool Succeeded { get; set; }
+    public Uri? QrCode { get; set; }
+    public string? ShortSignatureValue { get; set; }
+    public string? ExpeditionDate { get; set; }
+    public string? IssuerVatId { get; set; }
+    public string? Identifier { get; set; }
 }
