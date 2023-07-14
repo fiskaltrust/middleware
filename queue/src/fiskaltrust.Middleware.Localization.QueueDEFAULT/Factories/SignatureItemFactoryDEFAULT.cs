c7c346a17bb416ad1e9a586b17425b7b8d1274d5
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-07-14 14:04:42 +0200

    Fixes in SignatureItemFactoryDEFAULT.cs

--- queue/src/fiskaltrust.Middleware.Localization.QueueDEFAULT/Factories/SignatureItemFactoryDEFAULT.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueDEFAULT/Factories/SignatureItemFactoryDEFAULT.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using fiskaltrust.ifPOS.v1;
+using fiskaltrust.Middleware.Contracts.Factories;
 
 namespace fiskaltrust.Middleware.Localization.QueueDEFAULT.Factories
 {
-    public class SignatureFactoryDefault
+    public class SignatureItemFactoryDEFAULT : SignatureItemFactory
     {
+        public override long CountryBaseState => 000000000;
+
+        public SignatureItemFactoryDEFAULT() { 
+        }
+
+        protected static NumberFormatInfo CurrencyFormatter = new ()
+        {
+            NumberDecimalSeparator = ".",
+            NumberGroupSeparator = "",
+            CurrencyDecimalDigits = 2
+        };
+        
         public List<SignaturItem> GetSignaturesForPosReceiptTransaction()
         {
             var signatures = new List<SignaturItem>
@@ -21,6 +35,5 @@ namespace fiskaltrust.Middleware.Localization.QueueDEFAULT.Factories
             ftSignatureFormat = (long)SignaturItem.Formats.QR_Code,
             Data = data
         };
-        
     }
 }
