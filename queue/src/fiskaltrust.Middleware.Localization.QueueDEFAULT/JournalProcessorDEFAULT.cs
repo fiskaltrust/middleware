61ff1b0064fb051985876192d5f8f0e90f6110e0
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-07-17 14:07:34 +0200

    Improved return JournalProcessorDEFAULT

--- queue/src/fiskaltrust.Middleware.Localization.QueueDEFAULT/JournalProcessorDEFAULT.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueDEFAULT/JournalProcessorDEFAULT.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.Middleware.Contracts;
+using fiskaltrust.storage.V0;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace fiskaltrust.Middleware.Localization.QueueDEFAULT
 {
     public class JournalProcessorDEFAULT : IMarketSpecificJournalProcessor
     {
         private readonly ILogger<JournalProcessorDEFAULT> _logger;
+        private readonly IConfigurationRepository _configurationRepository;
 
         public JournalProcessorDEFAULT(
-            ILogger<JournalProcessorDEFAULT> logger)
+            ILogger<JournalProcessorDEFAULT> logger, IConfigurationRepository configurationRepository)
         {
             _logger = logger;
+            _configurationRepository = configurationRepository;
         }
 
-        public IAsyncEnumerable<JournalResponse> ProcessAsync(JournalRequest request)
+        public async IAsyncEnumerable<JournalResponse> ProcessAsync(JournalRequest request)
         {
+            var result = new { QueueDEList = await _configurationRepository.GetQueueDEListAsync() };
+            yield return new JournalResponse
+            {
+                Chunk = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)).ToList()
+            };
             throw new NotImplementedException();
         }
     }
