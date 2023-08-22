5314c01a054b5155c65774035bd87567f77eab02
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-08-22 14:35:08 +0200

    Created IJournalFRCopyPayloadRepository.cs

---
+++ queue/src/fiskaltrust.Middleware.Contracts/Repositories/FR/IJournalFRCopyPayloadRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace fiskaltrust.Middleware.Contracts.Repositories.FR
+{
+    public interface IJournalFRCopyPayloadRepository
+    {
+        int JournalFRGetCountOfCopies(string cbPreviousReceiptReference);
+
+        bool InsertJournalFRCopyPayload(ftJournalFRCopyPayload c);
+
+        bool HasJournalFRCopyPayloads();
+    }
+}
