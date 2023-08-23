01f327551babae821b3c20413912ea901a93f0a8
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-08-23 12:41:58 +0200

    Added proposal for a temporary solution

---
+++ queue/src/fiskaltrust.Middleware.Contracts/Repositories/FR/TempSpace/ftJournalFRCopyPayload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fiskaltrust.Middleware.Contracts.Repositories.FR.TempSpace
+{
+    public class ftJournalFRCopyPayload
+    {
+        public Guid QueueId { get; set; }
+        public string CashBoxIdentification { get; set; }
+        public string Siret { get; set; }
+        public string ReceiptId { get; set; }
+        public DateTime ReceiptMoment { get; set; }
+        public Guid QueueItemId { get; set; }
+        public string CopiedReceiptReference { get; set; }
+        public string CertificateSerialNumber { get; set; }
+        public long TimeStamp { get; set; }
+    }
+}
--- queue/src/fiskaltrust.Middleware.Storage.SQLite/Repositories/FR/SQLiteJournalFRCopyPayloadRepository.cs
+++ queue/src/fiskaltrust.Middleware.Storage.SQLite/Repositories/FR/SQLiteJournalFRCopyPayloadRepository.cs
@@ -1,46 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dapper;
-using fiskaltrust.Middleware.Contracts.Repositories.FR;
+using fiskaltrust.Middleware.Contracts.Repositories.FR.TempSpace;
 
 namespace fiskaltrust.Middleware.Storage.SQLite.Repositories.FR
 {
-    public class SQLiteJournalFRCopyPayloadRepository : AbstractSQLiteRepository<Guid, ftJournalFRCopyPayload>, IJournalFRCopyPayloadRepository
+    public class SQLiteJournalFRCopyPayloadRepository : AbstractSQLiteRepository<Guid, ftJournalFRCopyPayload>
     {
         public SQLiteJournalFRCopyPayloadRepository(ISqliteConnectionFactory connectionFactory, string path) : base(connectionFactory, path) { }
 
         public override void EntityUpdated(ftJournalFRCopyPayload entity) => entity.TimeStamp = DateTime.UtcNow.Ticks;
 
-        public async Task<int> JournalFRGetCountOfCopies(string cbPreviousReceiptReference)
-        {
-            var count = await DbConnection.ExecuteScalarAsync<int>(
+        public override async Task<ftJournalFRCopyPayload> GetAsync(Guid id) => 
+            await DbConnection.QueryFirstOrDefaultAsync<ftJournalFRCopyPayload>(
+                "SELECT * FROM ftJournalFRCopyPayload WHERE QueueItemId = @Id", 
+                new { Id = id }
+            ).ConfigureAwait(false);
+
+        public override async Task<IEnumerable<ftJournalFRCopyPayload>> GetAsync() => 
+            await DbConnection.QueryAsync<ftJournalFRCopyPayload>("SELECT * FROM ftJournalFRCopyPayload").ConfigureAwait(false);
+
+        public async Task<int> JournalFRGetCountOfCopies(string cbPreviousReceiptReference) =>
+            await DbConnection.ExecuteScalarAsync<int>(
                 "SELECT COUNT(*) FROM ftJournalFRCopyPayload WHERE CopiedReceiptReference = @Reference", 
                 new { Reference = cbPreviousReceiptReference }
             ).ConfigureAwait(false);
-            return count;
+
+        public async Task InsertAsync(ftJournalFRCopyPayload payload)
+        {
+            if (await GetAsync(GetIdForEntity(payload)).ConfigureAwait(false) != null)
+            {
+                throw new Exception("Entity with the same ID already exists.");
+            }
+
+            EntityUpdated(payload);
+            await DbConnection.ExecuteAsync(
+                "INSERT INTO ftJournalFRCopyPayload (QueueId, CashBoxIdentification, Siret, ReceiptId, ReceiptMoment, QueueItemId, CopiedReceiptReference, CertificateSerialNumber) " +
+                "Values (@QueueId, @CashBoxIdentification, @Siret, @ReceiptId, @ReceiptMoment, @QueueItemId, @CopiedReceiptReference, @CertificateSerialNumber);", 
+                payload
+            ).ConfigureAwait(false);
         }
 
-        public async Task<bool> InsertJournalFRCopyPayload(ftJournalFRCopyPayload payload)
-        {
-            try
-            {
-                var sql = "INSERT INTO ftJournalFRCopyPayload " +
-                              "(QueueId, CashBoxIdentification, Siret, ReceiptId, ReceiptMoment, QueueItemId, CopiedReceiptReference, CertificateSerialNumber) " +
-                              "Values (@QueueId, @CashBoxIdentification, @Siret, @ReceiptId, @ReceiptMoment, @QueueItemId, @CopiedReceiptReference, @CertificateSerialNumber);";
-                await DbConnection.ExecuteAsync(sql, payload).ConfigureAwait(false);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public async Task<bool> HasJournalFRCopyPayloads()
-        {
-            var count = await DbConnection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM ftJournalFRCopyPayload").ConfigureAwait(false);
-            return count > 0;
-        }
+        public async Task<bool> HasJournalFRCopyPayloads() =>
+            await DbConnection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM ftJournalFRCopyPayload").ConfigureAwait(false) > 0;
 
         protected override Guid GetIdForEntity(ftJournalFRCopyPayload entity) => entity.QueueItemId;
     }
