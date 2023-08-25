9c58d0c4f51cbcca56acd7d02e6bd1d07f66316f
Stefan Kert <stefan.kert@sbg.at>
2023-08-25 12:36:37 +0200

    added further step

---
+++ scu-it/src/fiskaltrust.Middleware.SCU.IT.CustomRTServer/SignatureTypesIT.cs
@@ -0,0 +1,10 @@
+namespace fiskaltrust.Middleware.SCU.IT.CustomRTServer
+{
+    public enum SignatureTypesIT
+    {
+        ReceiptNumber = 0x01,
+        ZNumber = 0x02,
+        ReceiptAmount = 0x03,
+        ReceiptTimestamp = 0x04
+    }
+}
