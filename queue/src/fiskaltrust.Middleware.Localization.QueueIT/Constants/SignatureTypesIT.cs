172538001399b44d2739af0007f69339b5fc3691
Stefan Kert <stefan.kert@sbg.at>
2023-08-31 20:12:18 +0200

    Update SignatureTypesIT.cs

--- queue/src/fiskaltrust.Middleware.Localization.QueueIT/Constants/SignatureTypesIT.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueIT/Constants/SignatureTypesIT.cs
@@ -9,6 +9,10 @@
         RTDocumentMoment = 0x13,
         RTDocumentType = 0x14,
         RTLotteryID = 0x15,
-        RTCustomerID = 0x16
+        RTCustomerID = 0x16,
+        RTReferenceZNumber = 0x17,
+        RTReferenceDocumentNumber = 0x18,
+        RTReferenceDocumentMoment = 0x19,
+        RTReferenceDocumentType = 0x20,
     }
 }
