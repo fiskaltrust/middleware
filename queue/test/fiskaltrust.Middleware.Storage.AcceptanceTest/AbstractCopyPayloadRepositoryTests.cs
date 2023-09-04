0af0cf894f6d4c7a317d8e95a951d7b769830656
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-09-04 12:41:43 +0200

    Added AbstractCopyPayloadRepositoryTests and SQLiteJournalFRCopyPayloadRepositoryTests classes
    - Abstracted common test logic into AbstractCopyPayloadRepositoryTests
    - Implemented SQLite-specific tests in SQLiteJournalFRCopyPayloadRepositoryTests for acceptance testing

---
+++ queue/test/fiskaltrust.Middleware.Storage.AcceptanceTest/AbstractCopyPayloadRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using fiskaltrust.Middleware.Contracts.Repositories.FR;
+using fiskaltrust.Middleware.Contracts.Repositories.FR.TempSpace;
+using Xunit;
+
+namespace fiskaltrust.Middleware.Storage.AcceptanceTest
+{
+    public abstract class AbstractCopyPayloadRepositoryTests
+    {
+        protected abstract Task<IJournalFRCopyPayloadRepository> CreateRepository();
+        protected abstract Task DisposeDatabase();
+
+        [Fact]
+        public async Task CanInsertAndRetrieveCopyPayload()
+        {
+            var repo = await CreateRepository();
+
+            var payload = new ftJournalFRCopyPayload
+            {
+                QueueId = Guid.NewGuid(),
+                CashBoxIdentification = "test",
+                Siret = "12345",
+                ReceiptId = "receipt1",
+                ReceiptMoment = DateTime.UtcNow,
+                QueueItemId = Guid.NewGuid(),
+                CopiedReceiptReference = "ref1",
+                CertificateSerialNumber = "cert123",
+                TimeStamp = DateTime.UtcNow.Ticks
+            };
+
+            var inserted = await repo.InsertAsync(payload);
+            var hasEntries = await repo.HasEntriesAsync();
+
+            Assert.True(inserted);
+            Assert.True(hasEntries);
+        }
+    }
+}
--- queue/test/fiskaltrust.Middleware.Storage.SQLite.AcceptanceTest/SQLiteJournalFRCopyPayloadRepositoryTests.cs
+++ queue/test/fiskaltrust.Middleware.Storage.SQLite.AcceptanceTest/SQLiteJournalFRCopyPayloadRepositoryTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using fiskaltrust.Middleware.Abstractions;
 using fiskaltrust.Middleware.Contracts.Repositories.FR;
 using fiskaltrust.Middleware.Contracts.Repositories.FR.TempSpace;
+using fiskaltrust.Middleware.Storage.AcceptanceTest;
 using fiskaltrust.Middleware.Storage.SQLite.Connection;
 using fiskaltrust.Middleware.Storage.SQLite.DatabaseInitialization;
 using fiskaltrust.Middleware.Storage.SQLite.Repositories.FR;
@@ -14,18 +15,17 @@ using Xunit;
 
 namespace fiskaltrust.Middleware.Storage.SQLite.AcceptanceTest
 {
-    public class SQLiteJournalFRCopyPayloadRepositoryTests : IDisposable
+    public class SQLiteJournalFRCopyPayloadRepositoryTests : AbstractCopyPayloadRepositoryTests, IDisposable
     {
         private readonly string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Guid.NewGuid().ToString());
         private SQLiteJournalFRCopyPayloadRepository _repo;
-        private readonly SqliteConnectionFactory _sqliteConnectionFactory = new SqliteConnectionFactory();
+        private readonly SqliteConnectionFactory _sqliteConnectionFactory = new();
 
         public SQLiteJournalFRCopyPayloadRepositoryTests()
         {
             Init().Wait();
         }
 
-        [Fact]
         public async Task Init()
         {
             var databasMigrator = new DatabaseMigrator(_sqliteConnectionFactory, 30 * 60, _path, new Dictionary<string, object>(), Mock.Of<ILogger<IMiddlewareBootstrapper>>());
@@ -34,38 +34,25 @@ namespace fiskaltrust.Middleware.Storage.SQLite.AcceptanceTest
             _repo = new SQLiteJournalFRCopyPayloadRepository(_sqliteConnectionFactory, _path);
         }
 
-        [Fact]
-        public async Task CanInsertAndRetrieveCopyPayload()
+        protected override Task<IJournalFRCopyPayloadRepository> CreateRepository()
         {
-            var payload = new ftJournalFRCopyPayload
-            {
-                QueueId = Guid.NewGuid(),
-                CashBoxIdentification = "test",
-                Siret = "12345",
-                ReceiptId = "receipt1",
-                ReceiptMoment = DateTime.UtcNow,
-                QueueItemId = Guid.NewGuid(),
-                CopiedReceiptReference = "ref1",
-                CertificateSerialNumber = "cert123",
-                TimeStamp = DateTime.UtcNow.Ticks
-            };
-
-            var inserted = await _repo.InsertAsync(payload);
-            var retrieved = await _repo.GetAsync(payload.QueueItemId);
-
-            Assert.True(inserted);
-            Assert.NotNull(retrieved);
-            Assert.Equal(payload.QueueItemId, retrieved.QueueItemId);
-            Assert.Equal(payload.CopiedReceiptReference, retrieved.CopiedReceiptReference);
+            return Task.FromResult<IJournalFRCopyPayloadRepository>(_repo ??= new SQLiteJournalFRCopyPayloadRepository(_sqliteConnectionFactory, _path));
         }
 
-        public void Dispose()
+        protected override Task DisposeDatabase()
         {
             _sqliteConnectionFactory.Dispose();
             if (File.Exists(_path))
             {
                 File.Delete(_path);
             }
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            DisposeDatabase().Wait();
         }
     }
 }
