096848aa32c7622aa83a5d99d802554bfd0d1e33
Stefan Kert <stefan.kert@sbg.at>
2023-09-06 16:02:49 +0200

    Ignoring test

--- scu-it/test/fiskaltrust.Middleware.SCU.IT.UnitTest/EpsonSCUTest.cs
+++ scu-it/test/fiskaltrust.Middleware.SCU.IT.UnitTest/EpsonSCUTest.cs
@@ -13,34 +13,34 @@ using fiskaltrust.ifPOS.v1;
 
 namespace fiskaltrust.Middleware.SCU.IT.UnitTest
 {
-    public class EpsonSCUTest
-    {
+    //public class EpsonSCUTest
+    //{
 
-        [Fact]
-        public async Task GetSerialNumber_GetResult_11DigetSerialnrAsync()
-        {
-            var config = new EpsonScuConfiguration()
-            {
-                DeviceUrl = "http://192.168.0.34"
-            };
+    //    [Fact]
+    //    public async Task GetSerialNumber_GetResult_11DigetSerialnrAsync()
+    //    {
+    //        var config = new EpsonScuConfiguration()
+    //        {
+    //            DeviceUrl = "http://192.168.0.34"
+    //        };
 
-            var epsonv2 = new EpsonCommunicationClientV2(new Mock<ILogger<EpsonSCU>>().Object, config, new Epson.Utilities.EpsonCommandFactory(config));
-            var epsonScu = new EpsonSCU(new Mock<ILogger<EpsonSCU>>().Object, config, new Epson.Utilities.EpsonCommandFactory(config), epsonv2);
+    //        var epsonv2 = new EpsonCommunicationClientV2(new Mock<ILogger<EpsonSCU>>().Object, config, new Epson.Utilities.EpsonCommandFactory(config));
+    //        var epsonScu = new EpsonSCU(new Mock<ILogger<EpsonSCU>>().Object, config, new Epson.Utilities.EpsonCommandFactory(config), epsonv2);
 
-            var processRequest = new ProcessRequest
-            {
-                ReceiptRequest = new ReceiptRequest
-                {
-                    ftReceiptCase = 0x4954_2000_0000_2000
-                },
-                ReceiptResponse = new ReceiptResponse
-                {
-                    ftCashBoxIdentification = "02020402",
-                    ftQueueID = Guid.NewGuid().ToString()
-                }
-            };
+    //        var processRequest = new ProcessRequest
+    //        {
+    //            ReceiptRequest = new ReceiptRequest
+    //            {
+    //                ftReceiptCase = 0x4954_2000_0000_2000
+    //            },
+    //            ReceiptResponse = new ReceiptResponse
+    //            {
+    //                ftCashBoxIdentification = "02020402",
+    //                ftQueueID = Guid.NewGuid().ToString()
+    //            }
+    //        };
 
-            _ = await epsonScu.ProcessReceiptAsync(processRequest);
-        }
-    }
+    //        _ = await epsonScu.ProcessReceiptAsync(processRequest);
+    //    }
+    //}
 }
