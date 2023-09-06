41c8daa28c757e65b0ce162b7b604afa90e4ca2c
Stefan Kert <stefan.kert@sbg.at>
2023-09-06 16:54:39 +0200

    Commented test

--- queue/test/fiskaltrust.Middleware.Localization.QueueIT.UnitTest/RequestCommandsTest/PosReceiptCommandTest.cs
+++ queue/test/fiskaltrust.Middleware.Localization.QueueIT.UnitTest/RequestCommandsTest/PosReceiptCommandTest.cs
@@ -21,76 +21,76 @@ namespace fiskaltrust.Middleware.Localization.QueueIT.UnitTest
     public class PosReceiptCommandTest
     {
 
-        [Fact(Skip = "")]
-        public async Task ExecuteAsync_RegisterInvoice_ValidResultAsync()
-        {
-            var request = new ReceiptRequest()
-            {
-                cbReceiptReference = "Reference007",
-                cbChargeItems = new[]
-                {
-                    new ChargeItem()
-                    {
-                        Description = "Testitem1",
-                        Amount = 9999.98m,
-                        ftChargeItemCase = 0x4954000000000001,
-                        Quantity= 2,
-                    },
-                    new ChargeItem()
-                    {
-                        Description = "Testitem2",
-                        Amount = 10,
-                        ftChargeItemCase = 0x4954000000000002,
-                        Quantity= 1,
-                    },
-                    new ChargeItem()
-                    {
-                        Description = "Discount 22% vat",
-                        Amount = -100,
-                        ftChargeItemCase = 0x4954000000000023,
-                    },
-                },
-                cbPayItems = new PayItem[]
-                {
-                    new PayItem(){
-                        Description = "Cash",
-                        Amount = 9909.98m,
-                        ftPayItemCase = 0x4954000000000001
-                    }
-                }
-            };
+        //[Fact(Skip = "")]
+        //public async Task ExecuteAsync_RegisterInvoice_ValidResultAsync()
+        //{
+        //    var request = new ReceiptRequest()
+        //    {
+        //        cbReceiptReference = "Reference007",
+        //        cbChargeItems = new[]
+        //        {
+        //            new ChargeItem()
+        //            {
+        //                Description = "Testitem1",
+        //                Amount = 9999.98m,
+        //                ftChargeItemCase = 0x4954000000000001,
+        //                Quantity= 2,
+        //            },
+        //            new ChargeItem()
+        //            {
+        //                Description = "Testitem2",
+        //                Amount = 10,
+        //                ftChargeItemCase = 0x4954000000000002,
+        //                Quantity= 1,
+        //            },
+        //            new ChargeItem()
+        //            {
+        //                Description = "Discount 22% vat",
+        //                Amount = -100,
+        //                ftChargeItemCase = 0x4954000000000023,
+        //            },
+        //        },
+        //        cbPayItems = new PayItem[]
+        //        {
+        //            new PayItem(){
+        //                Description = "Cash",
+        //                Amount = 9909.98m,
+        //                ftPayItemCase = 0x4954000000000001
+        //            }
+        //        }
+        //    };
 
-            var desscdMock = new Mock<IITSSCDProvider>();
-            desscdMock.Setup(x => x.ProcessReceiptAsync(It.IsAny<ProcessRequest>())).ReturnsAsync(new ProcessResponse
-            {
-                ReceiptResponse = new ReceiptResponse()
-            });
-            desscdMock.Setup(x => x.GetRTInfoAsync()).ReturnsAsync(new RTInfo
-            {
+        //    var desscdMock = new Mock<IITSSCDProvider>();
+        //    desscdMock.Setup(x => x.ProcessReceiptAsync(It.IsAny<ProcessRequest>())).ReturnsAsync(new ProcessResponse
+        //    {
+        //        ReceiptResponse = new ReceiptResponse()
+        //    });
+        //    desscdMock.Setup(x => x.GetRTInfoAsync()).ReturnsAsync(new RTInfo
+        //    {
 
-            });
+        //    });
 
 
-            var queueIt = new ftQueueIT() { CashBoxIdentification = "testserial", ftSignaturCreationUnitITId = Guid.NewGuid() };
+        //    var queueIt = new ftQueueIT() { CashBoxIdentification = "testserial", ftSignaturCreationUnitITId = Guid.NewGuid() };
       
-            var posReceiptCommand = new PointOfSaleReceipt0x0001(desscdMock.Object);
+        //    var posReceiptCommand = new PointOfSaleReceipt0x0001(desscdMock.Object);
 
-            var queue = new ftQueue() { ftQueueId = Guid.NewGuid(), ftReceiptNumerator = 5 };
-            var queueItem = new ftQueueItem() { ftQueueId = queue.ftQueueId, ftQueueItemId = Guid.NewGuid(), ftQueueRow = 7 };
+        //    var queue = new ftQueue() { ftQueueId = Guid.NewGuid(), ftReceiptNumerator = 5 };
+        //    var queueItem = new ftQueueItem() { ftQueueId = queue.ftQueueId, ftQueueItemId = Guid.NewGuid(), ftQueueRow = 7 };
 
-            var (receiptResponse, actionJournals) = await posReceiptCommand.ExecuteAsync(queue, queueIt, request, new ReceiptResponse(), queueItem);
+        //    var (receiptResponse, actionJournals) = await posReceiptCommand.ExecuteAsync(queue, queueIt, request, new ReceiptResponse(), queueItem);
 
-            var nrSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-number>").FirstOrDefault();
-            var znrSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<z-number>").FirstOrDefault();
-            var amntSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-amount>").FirstOrDefault();
-            var tsmpSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-timestamp>").FirstOrDefault();
+        //    var nrSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-number>").FirstOrDefault();
+        //    var znrSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<z-number>").FirstOrDefault();
+        //    var amntSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-amount>").FirstOrDefault();
+        //    var tsmpSig = receiptResponse.ftSignatures.Where(x => x.Caption == "<receipt-timestamp>").FirstOrDefault();
 
 
-            znrSig.Data.Should().Be("0");
-            nrSig.Data.Should().Be("245");
-            amntSig.Data.Should().Be("9909,98");
-            tsmpSig.Data.Should().Be("1999-01-01 00:00:01");
+        //    znrSig.Data.Should().Be("0");
+        //    nrSig.Data.Should().Be("245");
+        //    amntSig.Data.Should().Be("9909,98");
+        //    tsmpSig.Data.Should().Be("1999-01-01 00:00:01");
 
-        }
+        //}
     }
 }
