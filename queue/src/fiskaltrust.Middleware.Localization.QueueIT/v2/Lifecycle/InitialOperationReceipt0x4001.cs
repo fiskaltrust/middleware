35501bccd8e632764b07a8819ea3461541737da9
Stefan Kert <stefan.kert@sbg.at>
2023-09-21 06:48:06 +0200

    Improved

--- queue/src/fiskaltrust.Middleware.Localization.QueueIT/v2/Lifecycle/InitialOperationReceipt0x4001.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueIT/v2/Lifecycle/InitialOperationReceipt0x4001.cs
@@ -5,6 +5,7 @@ using fiskaltrust.ifPOS.v1;
 using fiskaltrust.ifPOS.v1.it;
 using fiskaltrust.Middleware.Contracts.Extensions;
 using fiskaltrust.Middleware.Localization.QueueIT.Constants;
+using fiskaltrust.Middleware.Localization.QueueIT.Extensions;
 using fiskaltrust.storage.V0;
 using Newtonsoft.Json;
 
@@ -25,45 +26,71 @@ namespace fiskaltrust.Middleware.Localization.QueueIT.v2.Lifecycle
 
         public async Task<(ReceiptResponse receiptResponse, List<ftActionJournal> actionJournals)> ExecuteAsync(ftQueue queue, ftQueueIT queueIt, ReceiptRequest request, ReceiptResponse receiptResponse, ftQueueItem queueItem)
         {
-            if (queue.IsNew())
+            try
             {
-                var scu = await _configurationRepository.GetSignaturCreationUnitITAsync(queueIt.ftSignaturCreationUnitITId.Value).ConfigureAwait(false);
-                var deviceInfo = await _itSSCD.GetRTInfoAsync().ConfigureAwait(false);
-                if (string.IsNullOrEmpty(scu.InfoJson))
+                if (queue.IsNew())
                 {
-                    scu.InfoJson = JsonConvert.SerializeObject(deviceInfo);
-                    await _configurationRepository.InsertOrUpdateSignaturCreationUnitITAsync(scu).ConfigureAwait(false);
+                    var scu = await _configurationRepository.GetSignaturCreationUnitITAsync(queueIt.ftSignaturCreationUnitITId.Value).ConfigureAwait(false);
+                    var deviceInfo = await _itSSCD.GetRTInfoAsync().ConfigureAwait(false);
+                    if (string.IsNullOrEmpty(scu.InfoJson))
+                    {
+                        scu.InfoJson = JsonConvert.SerializeObject(deviceInfo);
+                        await _configurationRepository.InsertOrUpdateSignaturCreationUnitITAsync(scu).ConfigureAwait(false);
+                    }
+
+                    var signature = SignaturItemFactory.CreateInitialOperationSignature(queueIt, deviceInfo);
+                    var actionJournal = ActionJournalFactory.CreateInitialOperationActionJournal(queue, queueItem, queueIt, request);
+                    queue.StartMoment = DateTime.UtcNow;
+
+                    var result = await _itSSCD.ProcessReceiptAsync(new ProcessRequest
+                    {
+                        ReceiptRequest = request,
+                        ReceiptResponse = receiptResponse,
+                    });
+
+                    if (result.ReceiptResponse.HasFailed())
+                    {
+                        return (result.ReceiptResponse, new List<ftActionJournal>
+                        {
+                            actionJournal
+                        });
+                    }
+                    await _configurationRepository.InsertOrUpdateQueueAsync(queue).ConfigureAwait(false);
+                    var signatures = new List<SignaturItem>
+                    {
+                        signature
+                    };
+                    signatures.AddRange(result.ReceiptResponse.ftSignatures);
+                    receiptResponse.ftSignatures = signatures.ToArray();
+
+                    return (receiptResponse, new List<ftActionJournal>
+                    {
+                        actionJournal
+                    });
                 }
-
-                var signature = SignaturItemFactory.CreateInitialOperationSignature(queueIt, deviceInfo);
-                var actionJournal = ActionJournalFactory.CreateInitialOperationActionJournal(queue, queueItem, queueIt, request);
-                queue.StartMoment = DateTime.UtcNow;
-
-                await _configurationRepository.InsertOrUpdateQueueAsync(queue).ConfigureAwait(false);
-                var result = await _itSSCD.ProcessReceiptAsync(new ProcessRequest
+                else
                 {
-                    ReceiptRequest = request,
-                    ReceiptResponse = receiptResponse,
-                });
-
+                    return (receiptResponse, new List<ftActionJournal>
+                    {
+                        ActionJournalFactory.CreateWrongStateForInitialOperationActionJournal(queue, queueItem, request)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
                 var signatures = new List<SignaturItem>
                 {
-                    signature
+                    new SignaturItem
+                    {
+                        Caption = "queue-initialoperation-generic-error",
+                        Data = $"{ex}",
+                        ftSignatureFormat = (long) SignaturItem.Formats.Text,
+                        ftSignatureType = 0x4954_2000_0000_3000
+                    }
                 };
-                signatures.AddRange(result.ReceiptResponse.ftSignatures);
                 receiptResponse.ftSignatures = signatures.ToArray();
-
-                return (receiptResponse, new List<ftActionJournal>
-                {
-                    actionJournal
-                });
-            }
-            else
-            {
-                return (receiptResponse, new List<ftActionJournal>
-                {
-                    ActionJournalFactory.CreateWrongStateForInitialOperationActionJournal(queue, queueItem, request)
-                });
+                receiptResponse.ftState = 0x4954_2001_EEEE_EEEE;
+                return (receiptResponse, new List<ftActionJournal>());
             }
         }
     }
