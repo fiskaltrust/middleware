33a43e807946b4381313a4e6025936adfde7e516
PawelKarczewski <karczewskipawel.j@gmail.com>
2023-09-29 00:05:39 +0200

    Quick fix JwtTestsHelper.cs

--- queue/test/fiskaltrust.Middleware.Localization.QueueFR.UnitTest/Helper/JwtTestsHelper.cs
+++ queue/test/fiskaltrust.Middleware.Localization.QueueFR.UnitTest/Helper/JwtTestsHelper.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Text;
+using fiskaltrust.Middleware.Localization.QueueFR.Models;
 using Newtonsoft.Json;
 
 namespace fiskaltrust.Middleware.Localization.QueueFR.UnitTest.Helper
 {
     public static class JwtTestHelper
     {
-        public static string GenerateJwt<T>(T payload)
+        public static string GenerateJwt(CopyPayload payload)
         {
             var header = Convert.ToBase64String(Encoding.UTF8.GetBytes("{}"));
             var body = Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload)));
