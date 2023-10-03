3d6a6f37f742156a5fec5fc8d8e86308cac13934
Paul Volavsek <paul.volavsek@gmail.com>
2023-10-03 17:16:01 +0200

    add ef migrations

---
+++ queue/src/fiskaltrust.Middleware.Storage.EF/Migrations/202310031515331_ftJournalFRCopyPayload.cs
@@ -0,0 +1,35 @@
+﻿namespace fiskaltrust.Middleware.Storage.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ftJournalFRCopyPayload : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ftJournalFRCopyPayload",
+                c => new
+                    {
+                        QueueItemId = c.Guid(nullable: false),
+                        QueueId = c.Guid(nullable: false),
+                        CashBoxIdentification = c.String(),
+                        Siret = c.String(),
+                        ReceiptId = c.String(),
+                        ReceiptMoment = c.DateTime(nullable: false, precision: 7, storeType: "datetime2"),
+                        CopiedReceiptReference = c.String(),
+                        CertificateSerialNumber = c.String(),
+                        TimeStamp = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.QueueItemId, clustered: false)
+                .Index(t => t.TimeStamp);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.ftJournalFRCopyPayload", new[] { "TimeStamp" });
+            DropTable("dbo.ftJournalFRCopyPayload");
+        }
+    }
+}
