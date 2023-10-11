2ddd246ae63640adfb7077f37669faa7a90fc22f
Stefan Kert <stefan.kert@sbg.at>
2023-10-11 13:27:25 +0200

    Cleanup

--- queue/src/fiskaltrust.Middleware.Localization.QueueIT/Extensions/ReceiptRequestExtensions.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueIT/Extensions/ReceiptRequestExtensions.cs
@@ -4,7 +4,6 @@ namespace fiskaltrust.Middleware.Localization.QueueIT.Extensions
 {
     public static class ReceiptRequestExtensions
     {
-
         public static bool IsVoid(this ReceiptRequest receiptRequest) => (receiptRequest.ftReceiptCase & 0x0000_0000_000F_0000) == 0x0000_0000_0004_0000;
 
         public static bool IsRefund(this ReceiptRequest receiptRequest) => (receiptRequest.ftReceiptCase & 0x0000_0000_0F00_0000) == 0x0000_0000_0100_0000;
