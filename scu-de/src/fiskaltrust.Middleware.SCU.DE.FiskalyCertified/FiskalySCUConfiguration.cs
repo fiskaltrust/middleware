d4e5ef9ae6f3a297ea9db90476155e84898ba96f
saschaknapp <47039335+saschaknapp@users.noreply.github.com>
2024-01-29 12:19:41 +0100

    Update FiskalySCUConfiguration.cs
    The certification BSI ID was wrong. The correct one for fiskaly is 490-2021 https://www.bsi.bund.de/SharedDocs/Zertifikate_TR/Technische_Sicherheitseinrichtungen/BSI-K-TR-0490-2021.html?nn=456560

--- scu-de/src/fiskaltrust.Middleware.SCU.DE.FiskalyCertified/FiskalySCUConfiguration.cs
+++ scu-de/src/fiskaltrust.Middleware.SCU.DE.FiskalyCertified/FiskalySCUConfiguration.cs
@@ -9,7 +9,7 @@ namespace fiskaltrust.Middleware.SCU.DE.FiskalyCertified
         public Guid TssId { get; set; }
         public string AdminPin { get; set; }
         public bool EnableTarFileExport { get; set; } = true;
-        public virtual string CertificationId { get; set; } = "BSI-K-TR-0403-2021";
+        public virtual string CertificationId { get; set; } = "BSI-K-TR-0490-2021";
         public bool DisplayCertificationIdAddition { get; set; } = false;
         public string CertificationIdAddition { get; set; }
         public string ApiEndpoint { get; set; } = "https://kassensichv-middleware.fiskaly.com/api/v2";
