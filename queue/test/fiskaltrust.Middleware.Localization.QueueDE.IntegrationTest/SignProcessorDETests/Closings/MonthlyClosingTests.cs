4b76a6ff27dadf0ac5884ac60c1e552ce0fb2ead
PawelKarczewski <karczewskipawel.j@gmail.com>
2024-03-07 00:09:44 +0100

    Update MonthlyClosingTests with new helper and tests

--- queue/test/fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest/SignProcessorDETests/Closings/MonthlyClosingTests.cs
+++ queue/test/fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest/SignProcessorDETests/Closings/MonthlyClosingTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProcessorDETests.Fixtures;
+using fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProcessorDETests.Helpers;
 using fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProcessorDETests.Receipts;
+using FluentAssertions;
 using Xunit;
 
 namespace fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProcessorDETests.Closings
@@ -10,21 +12,33 @@ namespace fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProces
     {
         private readonly ClosingTests _closingTests;
         private readonly ReceiptTests _receiptTests;
+        private readonly ReceiptProcessorHelper _receiptProcessorHelper;
 
         public MonthlyClosingTests(SignProcessorDependenciesFixture fixture)
         {
             _closingTests = new ClosingTests(fixture);
             _receiptTests = new ReceiptTests(fixture);
+            _receiptProcessorHelper = new ReceiptProcessorHelper(fixture.SignProcessor); 
         }
+        
         [Fact]
         public async Task MonthlyClosing_QueueIsActiv_CheckEntryInActionJournalResponseQueueQueueItem() => await _closingTests.ClosingTests_CheckEntryInActionJournalResponseQueueQueueItem("MonthlyClosingReceipt", $"Monthly-Closing receipt was processed.").ConfigureAwait(false);
         [Fact]
         public async Task MonthlyClosing_MasterDataChange_CheckAccountOutlet() => await _closingTests.ClosingTests_MasterDataChange_CheckAccountOutlet("MonthlyClosingReceipt", $"Monthly-Closing receipt was processed, and a master data update was performed.", 0x4445000108000005).ConfigureAwait(false);
+        
         [Fact]
-        public async Task MonthlyClosing_IsNoImplicitFlow_ExpectArgumentException() => await MonthlClosing_ExpectArgumentException(0x4445000000000005, "ReceiptCase {0:x} (Monthly-closing receipt) must use implicit-flow flag.").ConfigureAwait(false);
+        public async Task MonthlyClosing_IsNoImplicitFlow_ExpectErrorState()
+        {
+            var receiptRequest = TestObjectFactory.GetReceipt(Path.Combine("Data", "MonthlyClosingReceipt"));
+            receiptRequest.ftReceiptCase = 0x4445000000000005;
+            var response = await _receiptProcessorHelper.ProcessReceiptRequestAsync(receiptRequest);
+
+            Assert.Equal(0xEEEE_EEEE, response.ftState);
+        }
+
         [Fact]
-        public async Task MonthlyClosing_IsTraining_ExpectArgumentException() => await MonthlClosing_ExpectArgumentException(0x4445000100020005, "ReceiptCase {0:x} can not use 'TrainingMode' flag.").ConfigureAwait(false);
-        private async Task MonthlClosing_ExpectArgumentException(long receiptCase, string errorMessage)
+        public async Task MonthlyClosing_IsTraining_ExpectArgumentException() => await MonthlyClosing_ExpectArgumentException(0x4445000100020005, "ReceiptCase {0:x} can not use 'TrainingMode' flag.").ConfigureAwait(false);
+        private async Task MonthlyClosing_ExpectArgumentException(long receiptCase, string errorMessage)
         {
             var receiptRequest = TestObjectFactory.GetReceipt(Path.Combine("Data", "MonthlyClosingReceipt"));
             receiptRequest.ftReceiptCase = receiptCase;
