e41e357feda96dbfea43b83aa544e823cedd12b3
forsthug <85173816+forsthug@users.noreply.github.com>
2024-04-30 06:35:16 +0200

    Update queue/src/fiskaltrust.Middleware.Localization.QueueAT/RequestCommands/InitialOperationReceiptCommand.cs
    Co-authored-by: Tom Schmiedlechner <TSchmiedlechner@users.noreply.github.com>

--- queue/src/fiskaltrust.Middleware.Localization.QueueAT/RequestCommands/InitialOperationReceiptCommand.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueAT/RequestCommands/InitialOperationReceiptCommand.cs
@@ -125,7 +125,7 @@ namespace fiskaltrust.Middleware.Localization.QueueAT.RequestCommands
             actionJournals.Add(aj);
 
 
-            var (receiptIdentification, ftStateData, isBackupScuUsed, signatureItems, journalAT) = await SignReceiptAsync(queueAT, request, response.ftReceiptIdentification, response.ftReceiptMoment, queueItem.ftQueueItemId, true);
+            var (receiptIdentification, ftStateData, isBackupScuUsed, signatureItems, journalAT) = await SignReceiptAsync(queueAT, request, response.ftReceiptIdentification, response.ftReceiptMoment, queueItem.ftQueueItemId, isZeroReceipt: true);
             response.ftSignatures = response.ftSignatures.Concat(signatureItems).ToArray();
             response.ftReceiptIdentification = receiptIdentification;
             response.ftStateData = ftStateData;
