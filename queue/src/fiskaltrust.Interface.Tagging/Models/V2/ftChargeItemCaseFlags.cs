7a1906b91842426e06d8594f76b802ebbb58ce2a
forsthug <85173816+forsthug@users.noreply.github.com>
2024-05-28 12:44:52 +0200

    ChargeItem Cases & Flags

--- queue/src/fiskaltrust.Interface.Tagging/Models/V2/ftChargeItemCaseFlags.cs
+++ queue/src/fiskaltrust.Interface.Tagging/Models/V2/ftChargeItemCaseFlags.cs
@@ -6,6 +6,9 @@ namespace fiskaltrust.Interface.Tagging.Models.V2
     [FlagExtensions(OnType = typeof(ChargeItem), OnField = nameof(ChargeItem.ftChargeItemCase))]
     public enum ftChargeItemCaseFlags : long
     {
-
+        DownPayment = 0x0000_0000_0008_0000,
+        Returnable = 0x0000_0000_0010_0000,
+        DiscountOrExtraCharge = 0x0000_0000_0004_0000,
+        DownPaymentCharge = 0x0000_0000_0004_0000,
     }
 }
