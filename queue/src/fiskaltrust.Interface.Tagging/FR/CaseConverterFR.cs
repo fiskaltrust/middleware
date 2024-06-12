4bc8fe79823dbad5f4a45da31cc63bd54b451432
Leila <67684543+LeilaZarban@users.noreply.github.com>
2024-06-12 14:58:18 +0200

    update convertor

--- queue/src/fiskaltrust.Interface.Tagging/FR/CaseConverterFR.cs
+++ queue/src/fiskaltrust.Interface.Tagging/FR/CaseConverterFR.cs
@@ -5,6 +5,7 @@ using V2 = fiskaltrust.Interface.Tagging.Models.V2;
 using fiskaltrust.Interface.Tagging.Models.Extensions;
 using fiskaltrust.Interface.Tagging.Models.V2.Extensions;
 using fiskaltrust.Interface.Tagging.Models.V1.FR.Extensions;
+using fiskaltrust.ifPOS.v0;
 
 
 namespace fiskaltrust.Interface.Tagging.FR
@@ -25,41 +26,58 @@ namespace fiskaltrust.Interface.Tagging.FR
                 throw new Exception("Not a FR receipt case.");
             }
             var v2ftChargeItemCase = (V2.ftChargeItemCases) (chargeItem.GetV2ChargeItemCase() & 0xFFFF);
-            var v1ftChargeItemCase = v2ftChargeItemCase switch
+            V1.FR.ftChargeItemCases v1ftChargeItemCase;
+            if (chargeItem.IsV2Downpayment0x0008())
             {
-                V2.ftChargeItemCases.UnknownTypeOfService0x0000 => V1.FR.ftChargeItemCases.UnknownTypeOfService0x0000,
-                V2.ftChargeItemCases.UndefinedTypeOfServiceNormalVATRate0x0003 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceNormalVATRate0x0003,
-                V2.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate10x0001 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate10x0001,
-                V2.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate20x0002 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate20x0002,
-                V2.ftChargeItemCases.UndefinedTypeOfServiceSpecialVATRate10x0004 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceSpecialVATRate10x0004,
-                V2.ftChargeItemCases.UndefinedTypeOfServiceZeroVAT0x0007 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceZeroVAT0x0005,
-                V2.ftChargeItemCases.ReverseChargeReversalOfTaxLiability0x5000 => V1.FR.ftChargeItemCases.ReverseChargeReversalOfTaxLiability0x0006,
-                V2.ftChargeItemCases.NotOwnSales0x0060 => V1.FR.ftChargeItemCases.NotOwnSales0x0007,
-                V2.ftChargeItemCases.DeliveryNormalVATRate0x0013 => V1.FR.ftChargeItemCases.DeliveryNormalVATRate0x000A,
-                V2.ftChargeItemCases.DeliveryDiscountedVATRate10x0011 => V1.FR.ftChargeItemCases.DeliveryDiscountedVATRate10x0008,
-                V2.ftChargeItemCases.DeliveryDiscountedVATRate20x0012 => V1.FR.ftChargeItemCases.DeliveryDiscountedVATRate20x0009,
-                V2.ftChargeItemCases.DeliverySpecialVATRate10x0014 => V1.FR.ftChargeItemCases.DeliverySpecialVATRate10x000B,
-                V2.ftChargeItemCases.DeliveryZeroVAT0x0017 => V1.FR.ftChargeItemCases.DeliveryZeroVAT0x000C,
-                V2.ftChargeItemCases.OtherServicesNormalVATRate0x0023 => V1.FR.ftChargeItemCases.OtherServicesNormalVATRate0x000F,
-                V2.ftChargeItemCases.OtherServicesDiscountedVATRate10x0021 => V1.FR.ftChargeItemCases.OtherServicesDiscountedVATRate10x000D,
-                V2.ftChargeItemCases.OtherServicesDiscountedVATRate20x0022 => V1.FR.ftChargeItemCases.OtherServicesDiscountedVATRate20x000E,
-                V2.ftChargeItemCases.OtherServicesSpecialVATRate10x0024 => V1.FR.ftChargeItemCases.OtherServicesSpecialVATRate10x0010,
-                V2.ftChargeItemCases.OtherServicesZeroVAT0x0027 => V1.FR.ftChargeItemCases.OtherServicesZeroVAT0x0011,
-                V2.ftChargeItemCases.CatalogueServicesNormalVATRate0x0053 => V1.FR.ftChargeItemCases.CatalogueServicesNormalVATRate0x0014,
-                V2.ftChargeItemCases.CatalogueServicesDiscountedVATRate10x0051 => V1.FR.ftChargeItemCases.CatalogueServicesDiscountedVATRate10x0012,
-                V2.ftChargeItemCases.CatalogueServicesDiscountedVATRate20x0052 => V1.FR.ftChargeItemCases.CatalogueServicesDiscountedVATRate20x0013,
-                V2.ftChargeItemCases.CatalogueServicesSpecialVATRate10x0054 => V1.FR.ftChargeItemCases.CatalogueServicesSpecialVATRate10x0015,
-                V2.ftChargeItemCases.CatalogueServicesZeroVAT0x0057 => V1.FR.ftChargeItemCases.CatalogueServicesZeroVAT0x0016,
-                V2.ftChargeItemCases.OwnConsumptionNormalVATRate0x0073 => V1.FR.ftChargeItemCases.OwnConsumptionNormalVATRate0x0019,
-                V2.ftChargeItemCases.OwnConsumptionDiscountedVATRate10x0071 => V1.FR.ftChargeItemCases.OwnConsumptionDiscountedVATRate10x0017,
-                V2.ftChargeItemCases.OwnConsumptionDiscountedVATRate20x0072 => V1.FR.ftChargeItemCases.OwnConsumptionDiscountedVATRate20x0018,
-                V2.ftChargeItemCases.OwnConsumptionSpecialVATRate10x0074 => V1.FR.ftChargeItemCases.OwnConsumptionSpecialVATRate10x001A,
-                V2.ftChargeItemCases.OwnConsumptionZeroVAT0x0077 => V1.FR.ftChargeItemCases.OwnConsumptionZeroVAT0x001B,
+                var v2ftChargeItemVat = (V2.Vat) (chargeItem.GetV2Vat() & 0xF);
+                v1ftChargeItemCase = v2ftChargeItemVat switch
+                {
+                    V2.Vat.VatNormal0x3 => V1.FR.ftChargeItemCases.DownPaymentNormalVATRate0x001E,
+                    V2.Vat.VatDiscounted10x1 => V1.FR.ftChargeItemCases.DownPaymentDiscountedVATRate10x001C,
+                    V2.Vat.VatDiscounted20x2 => V1.FR.ftChargeItemCases.DownPaymentDiscountedVATRate20x001D,
+                    V2.Vat.VatSpecial10x4 => V1.FR.ftChargeItemCases.DownPaymentSpecialVATRate10x001F,
+                    V2.Vat.VatZero0x7 => V1.FR.ftChargeItemCases.DownPaymentZeroVAT0x0020,
+                    _ => throw new NotImplementedException()
+                };
+            }
+            else
+            {
+                v1ftChargeItemCase = v2ftChargeItemCase switch
+                {
+                    V2.ftChargeItemCases.UnknownTypeOfService0x0000 => V1.FR.ftChargeItemCases.UnknownTypeOfService0x0000,
+                    V2.ftChargeItemCases.UndefinedTypeOfServiceNormalVATRate0x0003 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceNormalVATRate0x0003,
+                    V2.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate10x0001 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate10x0001,
+                    V2.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate20x0002 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceDiscountedVATRate20x0002,
+                    V2.ftChargeItemCases.UndefinedTypeOfServiceSpecialVATRate10x0004 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceSpecialVATRate10x0004,
+                    V2.ftChargeItemCases.UndefinedTypeOfServiceZeroVAT0x0007 => V1.FR.ftChargeItemCases.UndefinedTypeOfServiceZeroVAT0x0005,
+                    V2.ftChargeItemCases.ReverseChargeReversalOfTaxLiability0x5000 => V1.FR.ftChargeItemCases.ReverseChargeReversalOfTaxLiability0x0006,
+                    V2.ftChargeItemCases.NotOwnSales0x0060 => V1.FR.ftChargeItemCases.NotOwnSales0x0007,
+                    V2.ftChargeItemCases.DeliveryNormalVATRate0x0013 => V1.FR.ftChargeItemCases.DeliveryNormalVATRate0x000A,
+                    V2.ftChargeItemCases.DeliveryDiscountedVATRate10x0011 => V1.FR.ftChargeItemCases.DeliveryDiscountedVATRate10x0008,
+                    V2.ftChargeItemCases.DeliveryDiscountedVATRate20x0012 => V1.FR.ftChargeItemCases.DeliveryDiscountedVATRate20x0009,
+                    V2.ftChargeItemCases.DeliverySpecialVATRate10x0014 => V1.FR.ftChargeItemCases.DeliverySpecialVATRate10x000B,
+                    V2.ftChargeItemCases.DeliveryZeroVAT0x0017 => V1.FR.ftChargeItemCases.DeliveryZeroVAT0x000C,
+                    V2.ftChargeItemCases.OtherServicesNormalVATRate0x0023 => V1.FR.ftChargeItemCases.OtherServicesNormalVATRate0x000F,
+                    V2.ftChargeItemCases.OtherServicesDiscountedVATRate10x0021 => V1.FR.ftChargeItemCases.OtherServicesDiscountedVATRate10x000D,
+                    V2.ftChargeItemCases.OtherServicesDiscountedVATRate20x0022 => V1.FR.ftChargeItemCases.OtherServicesDiscountedVATRate20x000E,
+                    V2.ftChargeItemCases.OtherServicesSpecialVATRate10x0024 => V1.FR.ftChargeItemCases.OtherServicesSpecialVATRate10x0010,
+                    V2.ftChargeItemCases.OtherServicesZeroVAT0x0027 => V1.FR.ftChargeItemCases.OtherServicesZeroVAT0x0011,
+                    V2.ftChargeItemCases.CatalogueServicesNormalVATRate0x0053 => V1.FR.ftChargeItemCases.CatalogueServicesNormalVATRate0x0014,
+                    V2.ftChargeItemCases.CatalogueServicesDiscountedVATRate10x0051 => V1.FR.ftChargeItemCases.CatalogueServicesDiscountedVATRate10x0012,
+                    V2.ftChargeItemCases.CatalogueServicesDiscountedVATRate20x0052 => V1.FR.ftChargeItemCases.CatalogueServicesDiscountedVATRate20x0013,
+                    V2.ftChargeItemCases.CatalogueServicesSpecialVATRate10x0054 => V1.FR.ftChargeItemCases.CatalogueServicesSpecialVATRate10x0015,
+                    V2.ftChargeItemCases.CatalogueServicesZeroVAT0x0057 => V1.FR.ftChargeItemCases.CatalogueServicesZeroVAT0x0016,
+                    V2.ftChargeItemCases.OwnConsumptionNormalVATRate0x0073 => V1.FR.ftChargeItemCases.OwnConsumptionNormalVATRate0x0019,
+                    V2.ftChargeItemCases.OwnConsumptionDiscountedVATRate10x0071 => V1.FR.ftChargeItemCases.OwnConsumptionDiscountedVATRate10x0017,
+                    V2.ftChargeItemCases.OwnConsumptionDiscountedVATRate20x0072 => V1.FR.ftChargeItemCases.OwnConsumptionDiscountedVATRate20x0018,
+                    V2.ftChargeItemCases.OwnConsumptionSpecialVATRate10x0074 => V1.FR.ftChargeItemCases.OwnConsumptionSpecialVATRate10x001A,
+                    V2.ftChargeItemCases.OwnConsumptionZeroVAT0x0077 => V1.FR.ftChargeItemCases.OwnConsumptionZeroVAT0x001B,
 
-                V2.ftChargeItemCases.AccountOfAThirdParty0x0068 => V1.FR.ftChargeItemCases.AccountOfAThirdParty0x0021,
-                V2.ftChargeItemCases.ObligationSigned0x0090 => V1.FR.ftChargeItemCases.ObligationSigned0x0090,
-                _ => throw new NotImplementedException(),
-            };
+                    V2.ftChargeItemCases.AccountOfAThirdParty0x0068 => V1.FR.ftChargeItemCases.AccountOfAThirdParty0x0021,
+                    V2.ftChargeItemCases.ObligationSigned0x0090 => V1.FR.ftChargeItemCases.ObligationSigned0x0090,
+                    _ => throw new NotImplementedException(),
+                };
+            }
             chargeItem.SetV1ChargeItemCase((long) v1ftChargeItemCase);
         }
         public void ConvertftJournalTypeToV1(JournalRequest journalRequest) => throw new NotImplementedException();
