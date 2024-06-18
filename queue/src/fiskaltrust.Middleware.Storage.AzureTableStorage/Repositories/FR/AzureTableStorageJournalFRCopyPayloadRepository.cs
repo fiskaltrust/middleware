36b53fc4143ac3e17c1ea2ee23edc0f1edb5a2f1
Paul Volavsek <paul.volavsek@gmail.com>
2024-06-18 12:52:28 +0200

    fix copypayload

---
+++ queue/src/fiskaltrust.Middleware.Storage.AzureTableStorage/Repositories/FR/AzureTableStorageJournalFRCopyPayloadRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Azure.Data.Tables;
+using fiskaltrust.Middleware.Contracts.Repositories;
+using fiskaltrust.Middleware.Storage.AzureTableStorage.Mapping;
+using fiskaltrust.Middleware.Storage.AzureTableStorage.TableEntities;
+using fiskaltrust.storage.V0;
+
+namespace fiskaltrust.Middleware.Storage.AzureTableStorage.Repositories.FR
+{
+    public class AzureTableStorageJournalFRCopyPayloadRepository
+    {
+        public const string TABLE_NAME = "JournalFRCopyPayload";
+
+    }
+}
+
