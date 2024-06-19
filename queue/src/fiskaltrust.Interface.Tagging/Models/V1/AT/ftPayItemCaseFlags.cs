abae0fa52ba15f0754264da7865404dee2277116
forsthug <85173816+forsthug@users.noreply.github.com>
2024-06-19 08:50:53 +0200

    fix tagging

--- queue/src/fiskaltrust.Interface.Tagging/Models/V1/AT/ftPayItemCaseFlags.cs
+++ queue/src/fiskaltrust.Interface.Tagging/Models/V1/AT/ftPayItemCaseFlags.cs
@@ -3,7 +3,7 @@ using fiskaltrust.Interface.Tagging.Generator;
 
 namespace fiskaltrust.Interface.Tagging.Models.V1.AT
 {
-    [FlagExtensions(OnType = typeof(ChargeItem), OnField = nameof(ChargeItem.ftChargeItemCase), Prefix = "V1", CaseName = "ChargeItemCaseFlag")]
+    [FlagExtensions(OnType = typeof(PayItem), OnField = nameof(PayItem.ftPayItemCase), Prefix = "V1", CaseName = "PayItemFlag")]
     public enum ftPayItemCaseFlags : long
     {
 
