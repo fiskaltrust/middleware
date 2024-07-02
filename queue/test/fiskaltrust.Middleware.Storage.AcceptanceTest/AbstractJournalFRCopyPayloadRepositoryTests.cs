a07cbb4f67c25827ef43c9a2a4257c433fae1480
Paul Volavsek <paul.volavsek@gmail.com>
2024-07-02 11:23:30 +0200

    fix copypayload acceptance tests

---
+++ queue/test/fiskaltrust.Middleware.Storage.AcceptanceTest/AbstractJournalFRCopyPayloadRepositoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using fiskaltrust.Middleware.Contracts.Models.FR;
+using fiskaltrust.Middleware.Contracts.Repositories.FR;
+using fiskaltrust.storage.V0;
+using FluentAssertions;
+using Xunit;
+
+namespace fiskaltrust.Middleware.Storage.AcceptanceTest
+{
+    public abstract class AbstractJournalFRCopyPayloadRepositoryTests : IDisposable
+    {
+        public abstract Task<IJournalFRCopyPayloadRepository> CreateRepository(IEnumerable<ftJournalFRCopyPayload> entries);
+
+        public virtual void DisposeDatabase() { return; }
+
+        public void Dispose() => DisposeDatabase();
+
+
+        [Fact]
+        public async Task InsertingSameEntityTwice_ShouldThrowException()
+        {
+            var entries = StorageTestFixtureProvider.GetFixture().CreateMany<ftJournalFRCopyPayload>(10);
+            var repo = await CreateRepository(entries);
+
+            await Assert.ThrowsAsync<Exception>(() => repo.InsertAsync(entries.First()));
+        }
+
+        [Fact]
+        public async Task CanInsertAndRetrieveCopyPayload()
+        {
+            var entries = StorageTestFixtureProvider.GetFixture().CreateMany<ftJournalFRCopyPayload>(10);
+            var entry = entries.First();
+            var repo = await CreateRepository(entries);
+
+            entry.Should().BeEquivalentTo(await repo.GetAsync(entry.QueueItemId));
+        }
+
+        [Fact]
+        public async Task GetCountOfCopiesAsync_ShouldReturnCorrectNumberOfCopies()
+        {
+            var entries = StorageTestFixtureProvider.GetFixture().CreateMany<ftJournalFRCopyPayload>(3).ToList();
+            entries[0].CopiedReceiptReference = "ref1";
+            entries[1].CopiedReceiptReference = "ref1";
+            entries[2].CopiedReceiptReference = "ref2";
+
+            var repo = await CreateRepository(entries);
+
+            var countReference1 = await repo.GetCountOfCopiesAsync("ref1");
+            var countReference2 = await repo.GetCountOfCopiesAsync("ref2");
+
+            Assert.Equal(2, countReference1);
+            Assert.Equal(1, countReference2);
+        }
+    }
+}
