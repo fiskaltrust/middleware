9dde2be5709f3484a6abacf4845c3b06c13297dc
Leila <67684543+LeilaZarban@users.noreply.github.com>
2024-09-19 11:12:07 +0200

    Update scu-de/src/fiskaltrust.Middleware.SCU.DE.SwissbitCloudV2/Helpers/HttpClientWrapper.cs
    Co-authored-by: Tom Schmiedlechner <TSchmiedlechner@users.noreply.github.com>

--- scu-de/src/fiskaltrust.Middleware.SCU.DE.SwissbitCloudV2/Helpers/HttpClientWrapper.cs
+++ scu-de/src/fiskaltrust.Middleware.SCU.DE.SwissbitCloudV2/Helpers/HttpClientWrapper.cs
@@ -131,7 +131,7 @@ namespace fiskaltrust.Middleware.SCU.DE.SwissbitCloudV2.Helpers
                     await PostAsync(requestUri, content, currentTry).ConfigureAwait(false);
                 }
                 var responseContent = await response.Content.ReadAsStringAsync();
-                throw new SwissbitCloudV2Exception($"Communication error ({response.StatusCode}) while setting TSS metadata ({requestUri}). Response: {responseContent}",
+                throw new SwissbitCloudV2Exception($"Communication error ({response.StatusCode}) while executing POST {requestUri}. Response: {responseContent}",
                     (int) response.StatusCode, requestUri);
             }
             return response;
