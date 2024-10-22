9b880aff31bf2b08f52bd63a81f75f5851265c4c
Paul Volavsek <paul.volavsek@gmail.com>
2024-10-22 09:56:31 +0200

    clarify

--- queue/src/fiskaltrust.Middleware.Localization.QueueES/Exports/VeriFactu/VeriFactuMapping.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueES/Exports/VeriFactu/VeriFactuMapping.cs
@@ -71,28 +71,30 @@ public class VeriFactuMapping
                 NumSerieFactura = receiptResponse.ftReceiptIdentification, // Maybe split from '#'
                 FechaExpedicionFactura = receiptRequest.cbReceiptMoment.ToString("dd-MM-yyy")
             },
-            // RefExterna = receiptRequest.ftQueueItemID, This field is described in the exel but not in the xsd files
-            NombreRazonEmisor = _masterData.Outlet.OutletName, // "Name and business name of the person required to issue the invoice."
+            // RefExterna = receiptRequest.ftQueueItemID, This field is described in the exel but not present in the xsd files
+            NombreRazonEmisor = _masterData.Account.AccountName, // "Name and business name of the person required to issue the invoice."
+            // ^ Not sure how this needs to be formated. Maybe we'll need some extra fields in the master data?
+            // Should this be the AccountName, or OutletName or sth from the Agencies?
             TipoFactura = receiptRequest.ftReceiptCase switch
             {
-                _ => ClaveTipoFacturaType.F1, // figure out
+                _ => ClaveTipoFacturaType.F1, // figure out which ones map to which ones
             },
             ImporteRectificacion = new DesgloseRectificacionType
             {
-                BaseRectificada = receiptRequest.cbChargeItems.Sum(chargeItem => chargeItem.GetVATAmount()).ToString("0.00"), // rounding/validation?
-                CuotaRectificada = null, // whats the difference between this and `BaseRectificada`
+                BaseRectificada = receiptRequest.cbChargeItems.Sum(chargeItem => chargeItem.GetVATAmount()).ToString("0.00"), // Do we need rounding for all the the decimals or should we fail if it's not in the range?
+                CuotaRectificada = null, // whats the difference between `CuotaRectificada` and `BaseRectificada`
             },
             Desglose = receiptRequest.cbChargeItems.Select(chargeItem => new DetalleType
             {
                 BaseImponibleOimporteNoSujeto = (chargeItem.Amount - chargeItem.GetVATAmount()).ToString("0.00"),
                 Item = chargeItem.ftChargeItemCase switch
                 {
-                    _ => CalificacionOperacionType.S1 // figure out
+                    _ => CalificacionOperacionType.S1 // figure out which ones map to which ones
                     // CalificacionOperacionType
                     // OperacionExentaType
                 }
             }).ToArray(),
-            CuotaTotal = receiptRequest.cbChargeItems.Sum(chargeItem => chargeItem.GetVATAmount()).ToString("0.00"), // is this correct? how should this differ from ImporteRectificacion
+            CuotaTotal = receiptRequest.cbChargeItems.Sum(chargeItem => chargeItem.GetVATAmount()).ToString("0.00"), // is this correct? how should this differ from `ImporteRectificacion`
             ImporteTotal = (receiptRequest.cbReceiptAmount ?? receiptRequest.cbChargeItems.Sum(chargeItem => chargeItem.Amount)).ToString("0.00"),
             Encadenamiento = new RegistroFacturacionAltaTypeEncadenamiento
             {
@@ -101,7 +103,7 @@ public class VeriFactuMapping
                     : new EncadenamientoFacturaAnteriorType
                     {
                         IDEmisorFactura = previous!.Value.id.IDEmisorFactura,
-                        // The `IDEmisorFactura` field needs to be the `IDFactura.IDEmisorFactura` of the previous receipt.
+                        // ^ The `IDEmisorFactura` field needs to be the `IDFactura.IDEmisorFactura` of the previous receipt.
                         // We could either save the last IDEmisorFactura in the queueES like this
                         // or have a change masterdata reciept where we udpate the masterdata and handle this case
                         NumSerieFactura = previous!.Value.id.NumSerieFactura,
@@ -111,12 +113,16 @@ public class VeriFactuMapping
             },
             SistemaInformatico = new SistemaInformaticoType
             {
-                NombreRazon = _masterData.PosSystems.FirstOrDefault()!.Brand, // Name and Companyname?
-                Item = null, // VatId of producing company 
-                IdSistemaInformatico = _masterData.PosSystems.FirstOrDefault()!.Type, // need to clarify
+                NombreRazon = _masterData.PosSystems.FirstOrDefault()!.Brand, // "Name and company name of the producing person or entity." 
+                // ^ The brand name is maybe not enough here?
+                Item = null, // VatId of producing company. We don't have that right now.
+                IdSistemaInformatico = _masterData.PosSystems.FirstOrDefault()!.Type, // "Identification code given by the producing person or entity to its computerised invoicing system (RIS) which, once installed, constitutes the RIS used. It should distinguish it from any other possible different RIS produced by the same producing person or entity. The possible restrictions to its values shall be detailed in the corresponding documentation in the AEAT electronic office (validations document...)."
+                // ^ Is this correct? does this need to be in a specific format or something registered with the government somewhere?
                 Version = _masterData.PosSystems.FirstOrDefault()!.SoftwareVersion,
-                NumeroInstalacion = null, // Installation number of the possystem. Unique number of possystems use by the issuing entity.
+                NumeroInstalacion = null, // "Installation number of the computerised invoicing system (RIS) used. It must be distinguished from any other possible RIS used for the invoicing of the person liable to issue invoices, i.e. from other possible past, present or future RIS installations used for the invoicing of the person liable to issue invoices, even if the same producer's RIS is used in these installations."
+                // ^ We don't have that right now.
             },
+            // ^ Which PosSystem from the list should we take? In de we just take the first one...
             FechaHoraHusoGenRegistro = receiptResponse.ftReceiptMoment,
             TipoHuella = TipoHuellaType.Item01
         };
@@ -141,7 +147,7 @@ public static class RegistroFacturacionAltaTypeExt
             .AppendFormat(GetValue("Huella", registroFacturacionAlta.Encadenamiento.Item is EncadenamientoFacturaAnteriorType alta
                 ? alta.Huella
                 : "S"))
-            .AppendFormat(GetValue("NumSerieFactura", registroFacturacionAlta.FechaHoraHusoGenRegistro.ToString("O"), separator: false))
+            .AppendFormat(GetValue("FechaHoraHusoGenRegistro", registroFacturacionAlta.FechaHoraHusoGenRegistro.ToString("yyyy-MM-ddThh:mm:sszzz"), separator: false))
             .ToString();
 
         var hash = SHA256.HashData(Encoding.UTF8.GetBytes(data));
