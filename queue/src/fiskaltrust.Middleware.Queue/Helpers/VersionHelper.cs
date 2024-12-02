53f46ff7d5d9575a797bdb1a174fc56439c616de
PawelKarczewski <karczewskipawel.j@gmail.com>
2024-12-02 13:24:50 +0100

    Updated SignProcessor to include processing version in queue items by utilizing the VersionHelper

---
+++ queue/src/fiskaltrust.Middleware.Queue/Helpers/VersionHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace fiskaltrust.Middleware.Queue.Helpers;
+
+public class VersionHelper
+{
+    public static string GetCurrentMiddlewareVersion()
+    {
+        var version = typeof(SignProcessor).Assembly.GetName().Version;
+        return version?.ToString();
+    }
+}
