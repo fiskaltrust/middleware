68a99c5ca43b66f1996872ad51eb1cd0de8cd425
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-04-02 14:28:42 +0200

    test4

--- queue/src/fiskaltrust.Middleware.Queue.InMemory/PosBootstrapper.cs
+++ queue/src/fiskaltrust.Middleware.Queue.InMemory/PosBootstrapper.cs
@@ -23,9 +23,9 @@ namespace fiskaltrust.Middleware.Queue.InMemory
             storageBootStrapper.ConfigureStorageServices(serviceCollection);
 
             var assemblyName = typeof(PosBootstrapper).Assembly.GetName();
-            //var versionAttribute = typeof(PosBootstrapper).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split(new char[] { '+', '-' })[0];
-            //var version = Version.TryParse(versionAttribute, out var result) ? new Version(result.Major, result.Minor, result.Build, 0) : new Version(assemblyName.Version.Major, assemblyName.Version.Minor, assemblyName.Version.Build, 0);
-            //assemblyName.Version = version;
+            var versionAttribute = typeof(PosBootstrapper).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split(new char[] { '+', '-' })[0];
+            var version = Version.TryParse(versionAttribute, out var result) ? new Version(result.Major, result.Minor, result.Build, 0) : new Version(assemblyName.Version.Major, assemblyName.Version.Minor, assemblyName.Version.Build, 0);
+            assemblyName.Version = version;
             Configuration.Add("assemblyinfo", new AssemblyInfo { Name = assemblyName.FullName, Version = new Version() });
 
             var queueBootstrapper = new QueueBootstrapper(Id, Configuration);
