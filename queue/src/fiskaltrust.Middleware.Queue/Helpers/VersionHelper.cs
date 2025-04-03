6ccbafce8042400f6fc51aaffbf4e93e8fb21c85
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-04-03 11:23:33 +0200

    resolve nullable

--- queue/src/fiskaltrust.Middleware.Queue/Helpers/VersionHelper.cs
+++ queue/src/fiskaltrust.Middleware.Queue/Helpers/VersionHelper.cs
@@ -7,13 +7,16 @@ public static class VersionHelper
 {
     public static string GetCurrentMiddlewareVersion(Type assemblyType)
     {
-        assemblyType = assemblyType ?? typeof(VersionHelper);
+        if(assemblyType == null)
+        {
+            return typeof(VersionHelper).Assembly.GetName().Version?.ToString();
+        }
         var assemblyName = assemblyType.Assembly.GetName();
         var versionAttribute = assemblyType.Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split(new char[] { '+', '-' })[0];
         var version = Version.TryParse(versionAttribute, out var result)
             ? new Version(result.Major, result.Minor, result.Build, 0)
         : new Version(assemblyName.Version.Major, assemblyName.Version.Minor, assemblyName.Version.Build, 0);
         
-        return version?.ToString();
+        return version.ToString();
     } 
 }
