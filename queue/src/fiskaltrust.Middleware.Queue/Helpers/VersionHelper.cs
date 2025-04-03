72cdd9fa70aeaa024c21a32baf45fdedc9a4713f
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-04-03 11:07:08 +0200

    fix tests

--- queue/src/fiskaltrust.Middleware.Queue/Helpers/VersionHelper.cs
+++ queue/src/fiskaltrust.Middleware.Queue/Helpers/VersionHelper.cs
@@ -7,8 +7,9 @@ public static class VersionHelper
 {
     public static string GetCurrentMiddlewareVersion(Type assemblyType)
     {
-        var assemblyName = assemblyType?.Assembly.GetName();
-        var versionAttribute = assemblyType?.Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split(new char[] { '+', '-' })[0];
+        assemblyType = assemblyType ?? typeof(VersionHelper);
+        var assemblyName = assemblyType.Assembly.GetName();
+        var versionAttribute = assemblyType.Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion.Split(new char[] { '+', '-' })[0];
         var version = Version.TryParse(versionAttribute, out var result)
             ? new Version(result.Major, result.Minor, result.Build, 0)
         : new Version(assemblyName.Version.Major, assemblyName.Version.Minor, assemblyName.Version.Build, 0);
