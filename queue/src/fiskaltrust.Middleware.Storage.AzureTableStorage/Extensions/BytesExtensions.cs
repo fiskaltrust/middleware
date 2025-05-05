eeedaf0e6d58ea59c7134a242f4f156caa308495
Paul Volavsek <paul@volavsek.com>
2025-05-05 13:50:26 +0200

    add oversized compression

---
+++ queue/src/fiskaltrust.Middleware.Storage.AzureTableStorage/Extensions/BytesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fiskaltrust.Middleware.Storage.AzureTableStorage.Extensions
+{
+    public static class BytesExtensions
+    {
+        public static IEnumerable<ArraySegment<byte>> Chunk(this byte[] source, int chunkSize)
+        {
+            for (var i = 0; i < source.Length; i += chunkSize)
+            {
+                var chunk = new ArraySegment<byte>(source, i, Math.Min(chunkSize, source.Length - i));
+                yield return chunk;
+            }
+        }
+    }
+}
