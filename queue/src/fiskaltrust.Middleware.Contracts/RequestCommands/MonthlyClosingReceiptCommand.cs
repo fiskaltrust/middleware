6fcbeccc40f7fb8815f1f51f365f736ff6303351
Paul Volavsek <paul@volavsek.com>
2025-05-22 08:53:19 +0200

    remove old commands

--- queue/src/fiskaltrust.Middleware.Contracts/RequestCommands/MonthlyClosingReceiptCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using fiskaltrust.storage.V0;
-
-namespace fiskaltrust.Middleware.Contracts.RequestCommands
-{
-    public abstract class MonthlyClosingReceiptCommand : ClosingReceiptCommand
-    {
-        protected override string ClosingReceiptName => "Monthly-Closing";
-    }
-}
