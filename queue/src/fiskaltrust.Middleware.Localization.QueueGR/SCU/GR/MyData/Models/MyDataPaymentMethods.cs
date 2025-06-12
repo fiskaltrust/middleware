917d72d7e9fc42f4d2ab487d2b8f0359a474d28e
Stefan Kert <stefan.kert@fiskaltrust.eu>
2025-06-12 09:10:26 +0200

    Using multi use voucher

--- queue/src/fiskaltrust.Middleware.Localization.QueueGR/SCU/GR/MyData/Models/MyDataPaymentMethods.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueGR/SCU/GR/MyData/Models/MyDataPaymentMethods.cs
@@ -6,7 +6,7 @@ public class MyDataPaymentMethods
     public const int DomesticPaymentAccount = 1;
     public const int ForeignPaymentsSpecialAccount = 2;
     public const int Cash = 3;
-    public const int Cheque = 4;
+    public const int Check = 4;
     public const int OnCredit = 5;
     public const int WebBanking = 6;
     public const int PosEPos = 7;
