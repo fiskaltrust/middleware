c28aba89fd363ff9a74a64076e9c849a8dac610c
Stefan Kert <stefan.kert@fiskaltrust.eu>
2025-06-27 22:02:00 +0200

    Only check if non cloudcashbox

--- queue/src/fiskaltrust.Middleware.Queue/Bootstrapper/QueueBootstrapper.cs
+++ queue/src/fiskaltrust.Middleware.Queue/Bootstrapper/QueueBootstrapper.cs
@@ -68,12 +68,11 @@ namespace fiskaltrust.Middleware.Queue.Bootstrapper
 
         private static async Task CreateConfigurationActionJournalAsync(MiddlewareConfiguration middlewareConfiguration, IMiddlewareQueueItemRepository queueItemRepository, IMiddlewareActionJournalRepository actionJournalRepository)
         {
-            // TODO - We should find another way for performing the migration
-            // In cloud interfaces it maybe makes no sense to check this so we can skip it?
-            //if (MigrationHelper.IsMigrationInProgress(queueItemRepository, actionJournalRepository))
-            //{
-            //    return;
-            //}
+            if(!middlewareConfiguration.IsCloudCashBox && MigrationHelper.IsMigrationInProgress(queueItemRepository, actionJournalRepository))
+            {
+                return;
+            }
+
             var configuration = new Dictionary<string, object>
             {
                 { "MachineName", Environment.MachineName },
