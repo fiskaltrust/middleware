fc2a4ca38d730ba7a6044898985256087af74c7e
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-07-04 09:58:30 +0200

    resolve tests

--- queue/test/fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest/SignProcessorDETests/Helpers/RequestCommandFactoryHelper.cs
+++ queue/test/fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest/SignProcessorDETests/Helpers/RequestCommandFactoryHelper.cs
@@ -77,6 +77,7 @@ namespace fiskaltrust.Middleware.Localization.QueueDE.IntegrationTest.SignProces
                 actionJournalRepository,
                 transactionPayloadFactory,
                 new RequestCommandFactory(services.BuildServiceProvider()),
+                null,
                 logger
             );
 
