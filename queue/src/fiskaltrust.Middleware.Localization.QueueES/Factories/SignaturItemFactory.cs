abf75d77e3f5dfc859efd1de1711aa07eb48cc1c
Paul Volavsek <paul@volavsek.com>
2025-07-15 11:31:17 +0200

    remove mapping from es queue

--- queue/src/fiskaltrust.Middleware.Localization.QueueES/Factories/SignaturItemFactory.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueES/Factories/SignaturItemFactory.cs
@@ -1,8 +1,7 @@
 ﻿using System.Web;
 using fiskaltrust.ifPOS.v2;
 using fiskaltrust.ifPOS.v2.Cases;
-using fiskaltrust.Middleware.Localization.QueueES.Models.Cases;
-using fiskaltrust.Middleware.SCU.ES.Models;
+using fiskaltrust.ifPOS.v2.es.Cases;
 using fiskaltrust.storage.V0;
 
 namespace fiskaltrust.Middleware.Localization.QueueES.Factories;
@@ -28,57 +27,6 @@ public static class SignaturItemFactory
             ftSignatureFormat = SignatureFormat.Text,
             Caption = $"Out-of-operation receipt",
             Data = $"Queue-ID: {queue.ftQueueId}"
-        };
-    }
-
-    public static SignatureItem[] CreateVeriFactuQRCode(string baseUrl, RegistroFacturacionAlta registroFacturacionAlta)
-    {
-        var query = HttpUtility.ParseQueryString(String.Empty);
-        query.Add("nif", registroFacturacionAlta.IDFactura.IDEmisorFactura);
-        query.Add("numserie", registroFacturacionAlta.IDFactura.NumSerieFactura);
-        query.Add("fecha", registroFacturacionAlta.IDFactura.FechaExpedicionFactura);
-        query.Add("importe", registroFacturacionAlta.ImporteTotal);
-
-        var uriBuider = new UriBuilder(baseUrl)
-        {
-            Query = query.ToString()
-        };
-
-        return [
-            new SignatureItem()
-            {
-                Caption = "QR tributario:",
-                Data = uriBuider.Uri.ToString(),
-                ftSignatureFormat = SignatureFormat.QRCode.WithPosition(SignatureFormatPosition.BeforeHeader),
-                ftSignatureType = SignatureTypeES.Url.As<SignatureType>(),
-            },
-            new SignatureItem {
-                Data = "Factura verificable en la Sede electrónica de la AEAT",
-                ftSignatureFormat = SignatureFormat.Text.WithPosition(SignatureFormatPosition.BeforeHeader),
-                ftSignatureType = SignatureType.Unknown,
-            }
-        ];
-    }
-
-    public static SignatureItem CreateESSignature(byte[] signature)
-    {
-        return new SignatureItem()
-        {
-            Caption = "Signature",
-            Data = Convert.ToBase64String(signature),
-            ftSignatureFormat = SignatureFormat.Base64,
-            ftSignatureType = SignatureTypeES.Signature.As<SignatureType>().WithFlag(SignatureTypeFlags.VisualizationOptional)
-        };
-    }
-
-    public static SignatureItem CreateESHuella(string huella)
-    {
-        return new SignatureItem()
-        {
-            Caption = "Huella",
-            Data = huella,
-            ftSignatureFormat = SignatureFormat.Text,
-            ftSignatureType = SignatureTypeES.Huella.As<SignatureType>()
         };
     }
 }
