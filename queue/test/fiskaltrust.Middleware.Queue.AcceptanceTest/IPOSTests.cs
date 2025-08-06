9743dfe660a404475f17dd88ecd97af8e6028198
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-08-06 14:03:28 +0200

    Update-tests-Get-free-port

--- queue/test/fiskaltrust.Middleware.Queue.AcceptanceTest/IPOSTests.cs
+++ queue/test/fiskaltrust.Middleware.Queue.AcceptanceTest/IPOSTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using AutoFixture;
 using fiskaltrust.ifPOS.v1;
 using fiskaltrust.Middleware.Contracts.Interfaces;
@@ -14,22 +15,22 @@ namespace fiskaltrust.Middleware.Queue.AcceptanceTest
     public class IPOSTests
     {
         private readonly string _host = "localhost";
-        private readonly int _port = 20044;
-
-        public Server StartHost(IPOS posInstance)
+        
+        public Server StartHost(IPOS posInstance,out int boundPort)
         {
             var server = new Server
             {
-                Ports = { new ServerPort(_host, _port, ServerCredentials.Insecure) }
+                Ports = { new ServerPort(_host, 0, ServerCredentials.Insecure) }
             };
             server.Services.AddCodeFirst(posInstance);
             server.Start();
+            boundPort = server.Ports.Single().BoundPort;
             return server;
         }
 
-        public IPOS GetClient()
+        public IPOS GetClient(int port)
         {
-            var channel = new Channel(_host, _port, ChannelCredentials.Insecure);
+            var channel = new Channel(_host, port, ChannelCredentials.Insecure);
             return channel.CreateGrpcService<IPOS>();
         }
 
@@ -42,8 +43,8 @@ namespace fiskaltrust.Middleware.Queue.AcceptanceTest
             var server = StartHost(new Queue(null, null, new Contracts.Models.MiddlewareConfiguration
             {
                 ProcessingVersion = "test"
-            }));
-            var client = GetClient();
+            }), out var boundPort);
+            var client = GetClient(boundPort);
 
             var echoResponse = await client.EchoAsync(echoRequest);
             echoResponse.Message.Should().Be(echoRequest.Message);
@@ -64,8 +65,8 @@ namespace fiskaltrust.Middleware.Queue.AcceptanceTest
             var server = StartHost(new Queue(mock.Object, null, new Contracts.Models.MiddlewareConfiguration()
             {
                 ProcessingVersion = "test"
-            }));
-            var client = GetClient();
+            }), out var boundPort);
+            var client = GetClient(boundPort);
 
             var signResponse = await client.SignAsync(receiptRequest);
 
