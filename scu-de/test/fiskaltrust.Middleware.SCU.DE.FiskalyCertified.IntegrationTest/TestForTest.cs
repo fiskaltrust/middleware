7d1a3fa2038900629af44bca21e0cb6b06005973
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-08-13 14:40:30 +0200

    add integration tests for scu.de

---
+++ scu-de/test/fiskaltrust.Middleware.SCU.DE.FiskalyCertified.IntegrationTest/TestForTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using AutoFixture;
+using fiskaltrust.ifPOS.v1.de;
+using fiskaltrust.Middleware.SCU.DE.Helpers.TLVLogParser.Logs;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using fiskaltrust.Middleware.SCU.DE.FiskalyCertified.Helpers;
+using fiskaltrust.Middleware.SCU.DE.FiskalyCertified.Services;
+
+namespace fiskaltrust.Middleware.SCU.DE.FiskalyCertified.IntegrationTest
+{
+    [Collection("FiskalySCUTests")]
+    public class TestForTest
+    {
+        
+        [Fact]
+        [Trait("TseCategory", "Cloud")]
+        public async Task StartTransactionAsync_Should_Return_Valid_Transaction_Result()
+        {
+            var apiSecret = Environment.GetEnvironmentVariable("APISECRET_FISKALYCERTIFIED_TESTS");
+            var apiKey = Environment.GetEnvironmentVariable("APIKEY_FISKALYCERTIFIED_TESTS");
+            var adminPin = Environment.GetEnvironmentVariable("ADMINPIN_FISKALYCERTIFIED_TESTS");
+            apiSecret.Should().Be("APISECRET_FISKALYCERTIFIED_TESTS");
+            apiKey.Should().Be("APIKEY_FISKALYCERTIFIED_TESTS");
+            adminPin.Should().Be("ADMINPIN_FISKALYCERTIFIED_TESTS");
+        }
+
+}
+}
