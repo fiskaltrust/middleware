c99967917607a99737a59be90ca0690df1d59a02
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-10-05 22:49:54 +0200

    initial models for BE and DK queues and signature creation units

---
+++ storage/src/fiskaltrust.storage/Models/ftQueueDK.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace fiskaltrust.storage.V0
+{
+    public class ftQueueDK
+    {
+        public Guid ftQueueDKId { get; set; }
+
+        public Guid? ftSignaturCreationUnitDKId { get; set; }
+
+        public string LastHash { get; set; }
+
+        public string LastSignature { get; set; }
+
+        public string CashBoxIdentification { get; set; }
+
+        public int SSCDFailCount { get; set; }
+
+        public DateTime? SSCDFailMoment { get; set; }
+
+        public Guid? SSCDFailQueueItemId { get; set; }
+
+        public int UsedFailedCount { get; set; }
+
+        public DateTime? UsedFailedMomentMin { get; set; }
+
+        public DateTime? UsedFailedMomentMax { get; set; }
+
+        public Guid? UsedFailedQueueItemId { get; set; }
+
+        public long TimeStamp { get; set; }
+        
+    }
+}
