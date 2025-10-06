ae8e0849642ac3f4e4d90ef05c49cb2d546dae0e
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-10-06 15:22:16 +0200

    refactor: remove unused properties from ftQueueBE and ftQueueDK

--- storage/src/fiskaltrust.storage/Models/ftQueueBE.cs
+++ storage/src/fiskaltrust.storage/Models/ftQueueBE.cs
@@ -8,25 +8,7 @@ namespace fiskaltrust.storage.V0
 
         public Guid? ftSignaturCreationUnitBEId { get; set; }
 
-        public string LastHash { get; set; }
-
-        public string LastSignature { get; set; }
-
         public string CashBoxIdentification { get; set; }
-
-        public int SSCDFailCount { get; set; }
-
-        public DateTime? SSCDFailMoment { get; set; }
-
-        public Guid? SSCDFailQueueItemId { get; set; }
-
-        public int UsedFailedCount { get; set; }
-
-        public DateTime? UsedFailedMomentMin { get; set; }
-
-        public DateTime? UsedFailedMomentMax { get; set; }
-
-        public Guid? UsedFailedQueueItemId { get; set; }
 
         public long TimeStamp { get; set; }
     }
