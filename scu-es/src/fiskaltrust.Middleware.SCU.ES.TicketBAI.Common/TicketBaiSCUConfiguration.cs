cc4e5e084c1da2fe69a10ff62499378bcf0f5583
Paul Volavsek <paul@volavsek.com>
2025-10-30 13:43:54 +0100

    fix config

--- scu-es/src/fiskaltrust.Middleware.SCU.ES.TicketBAI.Common/TicketBaiSCUConfiguration.cs
+++ scu-es/src/fiskaltrust.Middleware.SCU.ES.TicketBAI.Common/TicketBaiSCUConfiguration.cs
@@ -10,7 +10,31 @@ namespace fiskaltrust.Middleware.SCU.ES.TicketBAI.Common
         public X509Certificate2 Certificate { get; set; } = null!;
         public string EmisorNif { get; set; } = null!;
         public string EmisorApellidosNombreRazonSocial { get; set; } = null!;
+
+
         public static TicketBaiSCUConfiguration FromConfiguration(Dictionary<string, object> configuration)
-            => JsonSerializer.Deserialize<TicketBaiSCUConfiguration>(JsonSerializer.Serialize(configuration)) ?? new TicketBaiSCUConfiguration();
+        {
+            var config = new TicketBaiSCUConfiguration();
+
+            if (configuration.ContainsKey("CertificateBase64") && configuration["CertificateBase64"] != null &&
+                configuration.ContainsKey("CertificatePassword") && configuration["CertificatePassword"] != null)
+            {
+                config.Certificate = new X509Certificate2(
+                    Convert.FromBase64String(configuration["CertificateBase64"].ToString()!),
+                    configuration["CertificatePassword"].ToString()!);
+            }
+
+            if (configuration.ContainsKey("EmisorNif") && configuration["EmisorNif"] != null)
+            {
+                config.EmisorNif = configuration["EmisorNif"].ToString()!;
+            }
+
+            if (configuration.ContainsKey("EmisorApellidosNombreRazonSocial") && configuration["EmisorApellidosNombreRazonSocial"] != null)
+            {
+                config.EmisorApellidosNombreRazonSocial = configuration["EmisorApellidosNombreRazonSocial"].ToString()!;
+            }
+
+            return config;
+        }
     }
 }
