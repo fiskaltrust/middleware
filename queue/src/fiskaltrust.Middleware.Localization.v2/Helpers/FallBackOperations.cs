85af4a364f2de3de425dedb3ef96213a8c2a855f
Paul Volavsek <24523184+volllly@users.noreply.github.com>
2026-01-13 12:38:48 +0100

    Update queue/src/fiskaltrust.Middleware.Localization.v2/Helpers/FallBackOperations.cs
    Co-authored-by: Stefan Kert <stefan.kert@fiskaltrust.eu>

--- queue/src/fiskaltrust.Middleware.Localization.v2/Helpers/FallBackOperations.cs
+++ queue/src/fiskaltrust.Middleware.Localization.v2/Helpers/FallBackOperations.cs
@@ -9,5 +9,5 @@ public static class FallBackOperations
 
     public static Task<ProcessCommandResponse> NotSupported(ProcessCommandRequest request) => throw new NotSupportedException($"The ftReceiptCase {request.ReceiptRequest.ftReceiptCase.Case()} - 0x{request.ReceiptRequest.ftReceiptCase.Case():x} is not supported in the QueueBE implementation.");
 
-    public static Task<ProcessCommandResponse> NotYetImplemented(ProcessCommandRequest request) => throw new NotImplementedException($"The ftReceiptCase {request.ReceiptRequest.ftReceiptCase.Case()} - 0x{request.ReceiptRequest.ftReceiptCase.Case():x} is not yet implemented in the QueueBE implementation.");
+    public static Task<ProcessCommandResponse> NotYetImplemented(ProcessCommandRequest request) => throw new NotImplementedException($"The ftReceiptCase {request.ReceiptRequest.ftReceiptCase.Case()} - 0x{request.ReceiptRequest.ftReceiptCase.Case():x} is not yet implemented in the current implementation.");
 }
