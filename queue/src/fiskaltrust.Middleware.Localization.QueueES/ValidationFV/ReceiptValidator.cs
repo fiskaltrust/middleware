dc775ea694de3ebef13c2b4fb190651b7a4e6347
PawelKarczewski <karczewskipawel.j@gmail.com>
2026-02-05 12:53:20 +0100

    refactor: simplify validation handling by removing ReceiptRequestValidator and consolidating market-specific logic into individual ReceiptValidators

--- queue/src/fiskaltrust.Middleware.Localization.QueueES/ValidationFV/ReceiptValidator.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueES/ValidationFV/ReceiptValidator.cs
@@ -1,9 +1,14 @@
+using fiskaltrust.ifPOS.v2;
 using fiskaltrust.Middleware.Localization.v2.Validation;
+using FluentValidation;
+using ESValidations = fiskaltrust.Middleware.Localization.v2.Validation.Rules.ES;
 
 namespace fiskaltrust.Middleware.Localization.QueueES.ValidationFV;
 
 public class ReceiptValidator : MarketValidator
 {
-    protected override string RuleSetName => RuleSetNames.ES;
+    protected override IEnumerable<IValidator<ReceiptRequest>> GetMarketValidators()
+    {
+        yield return new ESValidations.ChargeItemValidations();
+    }
 }
-    
