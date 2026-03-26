6880210d0dc3692621bf69e48b9738975293b282
Stefan Kert <stefan.kert@fiskaltrust.eu>
2026-03-27 00:50:25 +0100

    Fix

--- scu-gr/src/fiskaltrust.Middleware.SCU.GR.MyData/ReceiptCaseFlagsGR.cs
+++ scu-gr/src/fiskaltrust.Middleware.SCU.GR.MyData/ReceiptCaseFlagsGR.cs
@@ -5,13 +5,6 @@ namespace fiskaltrust.Middleware.SCU.GR.Abstraction;
 public enum ReceiptCaseFlagsGR : long
 {
     HasTransportInformation = 0x0000_0000_0400_0000,
-
-    /// <summary>
-    /// When set on a ReceiptCase.Pay0x3005 receipt, instructs the SCU to call the
-    /// myDATA SendPaymentsMethod endpoint for an already-submitted invoice.
-    /// Full receipt case: 0x4752_2000_0800_3005
-    /// </summary>
-    SendPaymentsMethod = 0x0000_0000_0800_0000
 }
 
 public static class ReceiptCaseFlagsGRExt
