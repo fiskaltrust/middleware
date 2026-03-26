f2b578f90591ab647ae321ab1c367512bb438be6
Stefan Kert <stefan.kert@fiskaltrust.eu>
2026-03-27 00:27:08 +0100

    Fix

--- queue/src/fiskaltrust.Middleware.Localization.QueueGR/Processors/ProtocolCommandProcessorGR.cs
+++ queue/src/fiskaltrust.Middleware.Localization.QueueGR/Processors/ProtocolCommandProcessorGR.cs
@@ -29,7 +29,23 @@ public class ProtocolCommandProcessorGR(IGRSSCD sscd, IQueueStorageProvider queu
         return await Task.FromResult(new ProcessCommandResponse(response.ReceiptResponse, new List<ftActionJournal>())).ConfigureAwait(false);
     }
 
-    public async Task<ProcessCommandResponse> Pay0x3005Async(ProcessCommandRequest request) => await GRFallBackOperations.NoOp(request);
+    public async Task<ProcessCommandResponse> Pay0x3005Async(ProcessCommandRequest request)
+    {
+        var hasLocalPayItemFlag = request.ReceiptRequest.cbPayItems.Any(p => ((long) p.ftPayItemCase & 0x0000_0001_0000_0000) != 0);
+        if (!hasLocalPayItemFlag || request.ReceiptRequest.cbPreviousReceiptReference == null)
+        {
+            return await GRFallBackOperations.NoOp(request);
+        }
+
+        var receiptReferences = await _queueStorageProvider.GetReceiptReferencesIfNecessaryAsync(request);
+        var response = await _sscd.ProcessReceiptAsync(new ProcessRequest
+        {
+            ReceiptRequest = request.ReceiptRequest,
+            ReceiptResponse = request.ReceiptResponse,
+        }, receiptReferences);
+        return await Task.FromResult(new ProcessCommandResponse(response.ReceiptResponse, new List<ftActionJournal>())).ConfigureAwait(false);
+    }
+
 
     public async Task<ProcessCommandResponse> CopyReceiptPrintExistingReceipt0x3010Async(ProcessCommandRequest request) => await GRFallBackOperations.NoOp(request);
 }
